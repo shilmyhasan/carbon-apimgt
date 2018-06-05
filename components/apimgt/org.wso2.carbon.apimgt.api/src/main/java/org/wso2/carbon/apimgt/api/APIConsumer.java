4569404dbbe9401eb7cfe77ae02d8f2500ce0643
Tharindu Dharmarathna <tharindua@wso2.com>
2018-06-05 21:58:38 +0530

    forward porting fix to AMWAYSUB-88 for CAPAYMENTSPROD-133 APIM 2.1.0

--- components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/APIConsumer.java
+++ components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/APIConsumer.java
@@ -248,6 +248,19 @@ public interface APIConsumer extends APIManager {
             throws APIManagementException;
 
     /**
+     * Add new Subscriber
+     *
+     * @param identifier    APIIdentifier
+     * @param userId        id of the user
+     * @param applicationId Application Id
+     * @param groupId groupId of user
+     * @return SubscriptionResponse subscription response object
+     * @throws APIManagementException if failed to add subscription details to database
+     */
+    SubscriptionResponse addSubscription(APIIdentifier identifier, String userId, int applicationId,String groupId)
+            throws APIManagementException;
+
+    /**
      * 
      * @param subscriptionId id of the subscription
      * @return
@@ -264,6 +277,18 @@ public interface APIConsumer extends APIManager {
      * @throws APIManagementException if failed to remove subscription details from database
      */
     void removeSubscription(APIIdentifier identifier, String userId, int applicationId) throws APIManagementException;
+
+    /**
+     * Unsubscribe the specified user from the specified API in the given application
+     *
+     * @param identifier    APIIdentifier
+     * @param userId        id of the user
+     * @param applicationId Application Id
+     * @param groupId groupId of user
+     * @throws APIManagementException if failed to remove subscription details from database
+     */
+    void removeSubscription(APIIdentifier identifier, String userId, int applicationId,String groupId) throws APIManagementException;
+
 
     /** Removes a subscription specified by SubscribedAPI object
      * 
--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/UserAwareAPIConsumer.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/UserAwareAPIConsumer.java
@@ -74,6 +74,13 @@ public class UserAwareAPIConsumer extends APIConsumerImpl {
     }
 
     @Override
+    public void removeSubscription(APIIdentifier identifier, String userId, int applicationId, String groupId) throws
+            APIManagementException {
+        checkSubscribePermission();
+        super.removeSubscription(identifier, userId, applicationId, groupId);
+    }
+
+    @Override
     public void removeSubscription(SubscribedAPI subscription) throws APIManagementException {
         checkSubscribePermission();
         super.removeSubscription(subscription);
