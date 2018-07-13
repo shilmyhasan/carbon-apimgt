80c34f7ff758594407e864cbfd8e337706ae2161
ChamNDeSilva <chamalee@wso2.com>
2018-07-13 15:39:13 +0530

    Reverting the nimbus version change

--- components/oidc-authenticator/org.wso2.carbon.apimgt.hostobjects.oidc/src/main/java/org/wso2/carbon/apimgt/hostobjects/oidc/OIDCRelyingPartyObject.java
+++ components/oidc-authenticator/org.wso2.carbon.apimgt.hostobjects.oidc/src/main/java/org/wso2/carbon/apimgt/hostobjects/oidc/OIDCRelyingPartyObject.java
@@ -370,7 +370,7 @@ public class OIDCRelyingPartyObject extends ScriptableObject {
 
         boolean isSignatureValid;
         JWT idToken = JWTParser.parse(oidcAuthenticationToken.getIdTokenValue());
-        JWTClaimsSet idClaims = idToken.getJWTClaimsSet();
+        ReadOnlyJWTClaimsSet idClaims = idToken.getJWTClaimsSet();
 
         // Supports only signedJWT
         if (idToken instanceof SignedJWT) {
