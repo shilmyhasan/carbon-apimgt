a954915f67d7f2e1ccc69cbcb2cf3608f383f133
sameera910409 <sameera910409@gmail.com>
2018-09-21 08:55:30 +0530

    Add unit test cases for different wsdl element mapping with swagger

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/soaptorest/WSDL11SOAPOperationExtractor.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/soaptorest/WSDL11SOAPOperationExtractor.java
@@ -697,4 +697,8 @@ public class WSDL11SOAPOperationExtractor implements WSDLSOAPOperationExtractor 
         }
         return false;
     }
+
+    public Map<String, ModelImpl> getParameterModelMap() {
+        return parameterModelMap;
+    }
 }
