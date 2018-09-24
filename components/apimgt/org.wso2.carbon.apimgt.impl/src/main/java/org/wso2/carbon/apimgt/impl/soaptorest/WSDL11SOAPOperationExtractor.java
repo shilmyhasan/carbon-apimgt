5ed395553621174bc48231dde5821c04f00c642a
nalakas <nalakas@wso2.com>
2018-09-24 18:23:07 +0530

    refactoring

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/soaptorest/WSDL11SOAPOperationExtractor.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/soaptorest/WSDL11SOAPOperationExtractor.java
@@ -635,8 +635,8 @@ public class WSDL11SOAPOperationExtractor implements WSDLSOAPOperationExtractor 
                 wsdlOperation.setTargetNamespace(targetNamespace);
                 wsdlOperation.setStyle(soapOperation.getStyle());
 
-                wsdlOperation.setInputParameterModel(getSoapInputParameterModel(bindingOperation, soapOperation.getStyle()));
-                wsdlOperation.setOutputParameterModel(getSoapOutputParameterModel(bindingOperation, soapOperation.getStyle()));
+                wsdlOperation.setInputParameterModel(getSoapInputParameterModel(bindingOperation));
+                wsdlOperation.setOutputParameterModel(getSoapOutputParameterModel(bindingOperation));
             }
         }
         return wsdlOperation;
@@ -649,7 +649,7 @@ public class WSDL11SOAPOperationExtractor implements WSDLSOAPOperationExtractor 
      * @return list of swagger models for the parameters
      * @throws APIMgtWSDLException
      */
-    private List<ModelImpl> getSoapInputParameterModel(BindingOperation bindingOperation, String soapStyle) throws APIMgtWSDLException {
+    private List<ModelImpl> getSoapInputParameterModel(BindingOperation bindingOperation) throws APIMgtWSDLException {
 
         List<ModelImpl> inputParameterModelList = new ArrayList<>();
         Operation operation = bindingOperation.getOperation();
@@ -698,7 +698,7 @@ public class WSDL11SOAPOperationExtractor implements WSDLSOAPOperationExtractor 
      * @return list of swagger models for the parameters
      * @throws APIMgtWSDLException
      */
-    private List<ModelImpl> getSoapOutputParameterModel(BindingOperation bindingOperation, String soapStyle) throws APIMgtWSDLException {
+    private List<ModelImpl> getSoapOutputParameterModel(BindingOperation bindingOperation) throws APIMgtWSDLException {
         List<ModelImpl> outputParameterModelList = new ArrayList<>();
         Operation operation = bindingOperation.getOperation();
         if (operation != null) {
