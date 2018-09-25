49542c6b29d2596814d1239e2670ec6a0eab5acc
Samitha Chathuranga <rmschathuranga@gmail.com>
2018-09-25 14:09:18 +0530

    Check whther the headers are null SynapsePropertiesHandler

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/common/SynapsePropertiesHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/common/SynapsePropertiesHandler.java
@@ -42,8 +42,10 @@ public class SynapsePropertiesHandler extends AbstractHandler{
                 ((Axis2MessageContext) messageContext).getAxis2MessageContext();
         Map headers =
                 (Map) (axis2MsgContext).getProperty(org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS);
-        boolean isContentTypeNotSet = headers.get("Content-Type") == null || headers.get("Content-Type").equals("");
-
+        boolean isContentTypeNotSet = false;
+        if (headers != null) {
+            isContentTypeNotSet = headers.get("Content-Type") == null || headers.get("Content-Type").equals("");
+        }
         if (isContentTypeNotSet && (httpMethod.equals(Constants.Configuration.HTTP_METHOD_POST) ||
                 httpMethod.equals(Constants.Configuration.HTTP_METHOD_PUT))) {
             ((Axis2MessageContext) messageContext).getAxis2MessageContext().
