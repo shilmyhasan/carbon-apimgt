a1c6fb035d217101dc07537bc9ade6a9142eb3c2
chamilaadhi <cka.adhi@gmail.com>
2018-09-25 13:41:12 +0530

    Fix for LANTSUB-17

---
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/test/java/org/wso2/carbon/apimgt/impl/util/test/APIUtilTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.apimgt.impl.util.test;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.commons.collections.SetUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.juddi.v3.error.RegistryException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.wso2.carbon.apimgt.api.APIManagementException;
+import org.wso2.carbon.apimgt.impl.internal.ServiceReferenceHolder;
+import org.wso2.carbon.apimgt.impl.utils.APIUtil;
+import org.wso2.carbon.user.api.UserRealm;
+import org.wso2.carbon.user.api.UserStoreManager;
+import org.wso2.carbon.user.core.service.RealmService;
+import org.wso2.carbon.user.core.tenant.TenantManager;
+
+import junit.framework.TestCase;
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( {LogFactory.class, ServiceReferenceHolder.class, SSLSocketFactory.class})
+@PowerMockIgnore("javax.net.ssl.*")
+public class APIUtilTest extends TestCase{
+    
+    @Test
+    public void testIsRoleNameExist() throws Exception {
+        String userName = "John";
+        String roleName = "developer";
+
+        ServiceReferenceHolder serviceReferenceHolder = Mockito.mock(ServiceReferenceHolder.class);
+        RealmService realmService = Mockito.mock(RealmService.class);
+        TenantManager tenantManager = Mockito.mock(TenantManager.class);
+        UserRealm userRealm = Mockito.mock(UserRealm.class);
+        UserStoreManager userStoreManager = Mockito.mock(UserStoreManager.class);
+
+        PowerMockito.mockStatic(ServiceReferenceHolder.class);
+        Mockito.when(ServiceReferenceHolder.getInstance()).thenReturn(serviceReferenceHolder);
+        Mockito.when(serviceReferenceHolder.getRealmService()).thenReturn(realmService);
+        Mockito.when(realmService.getTenantManager()).thenReturn(tenantManager);
+        Mockito.when(realmService.getTenantUserRealm(Mockito.anyInt())).thenReturn(userRealm);
+        Mockito.when(userRealm.getUserStoreManager()).thenReturn(userStoreManager);
+        Mockito.when(userStoreManager.isExistingRole(roleName)).thenReturn(true);
+
+        Assert.assertTrue(APIUtil.isRoleNameExist(userName, roleName));
+        
+        Assert.assertFalse(APIUtil.isRoleNameExist(userName, "NonExistingDomain/role"));
+        Assert.assertFalse(APIUtil.isRoleNameExist(userName, "NonExistingDomain/"));
+        Assert.assertTrue(APIUtil.isRoleNameExist(userName, ""));//allow adding empty role
+    }
+
+}
