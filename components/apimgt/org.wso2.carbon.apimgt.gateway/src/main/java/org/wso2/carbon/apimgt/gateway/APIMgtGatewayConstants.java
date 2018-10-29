2b127a4ad015352b29b952ff296b280287be43dc
arshardh <arshardhifthikar50@gmail.com>
2018-10-29 12:24:09 +0530

    add correlation id to MDC when logging

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/APIMgtGatewayConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/APIMgtGatewayConstants.java
@@ -61,6 +61,7 @@ public class APIMgtGatewayConstants {
     public static final String RESPONSE_MEDIATION_LATENCY = "response_mediation_latency";
     public static final String OTHER_LATENCY = "other_latency";
     public static final String AM_CORRELATION_ID = "am.correlationID";
+    public static final String AM_ACTIVITY_ID = "activityid";
 
     /**
      * Constants for regex protector.
