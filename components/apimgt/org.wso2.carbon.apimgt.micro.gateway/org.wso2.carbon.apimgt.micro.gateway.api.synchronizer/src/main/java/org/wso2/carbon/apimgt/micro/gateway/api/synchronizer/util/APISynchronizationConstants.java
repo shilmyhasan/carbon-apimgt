1715cb97d5d6dad6ef5bbee584fbeec90b96595b
npamudika <rpnpamudika@gmail.com>
2019-01-25 12:31:03 +0530

    Update micro gateway web app version

--- components/apimgt/org.wso2.carbon.apimgt.micro.gateway/org.wso2.carbon.apimgt.micro.gateway.api.synchronizer/src/main/java/org/wso2/carbon/apimgt/micro/gateway/api/synchronizer/util/APISynchronizationConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.micro.gateway/org.wso2.carbon.apimgt.micro.gateway.api.synchronizer/src/main/java/org/wso2/carbon/apimgt/micro/gateway/api/synchronizer/util/APISynchronizationConstants.java
@@ -31,7 +31,7 @@ public class APISynchronizationConstants {
     public static final String API_VIEW_PATH = "/api/am/publisher/{version}/apis";
     public static final String API_VIEW_MEDIATION_POLICY_PATH = "/policies/mediation";
     public static final String API_VIEW_GLOBAL_MEDIATION_POLICY_PATH = "/api/am/publisher/{version}/policies/mediation";
-    public static final String DEFAULT_API_UPDATE_SERVICE_URL = "https://localhost:9443/micro-gateway/v0.9/updated-apis";
+    public static final String DEFAULT_API_UPDATE_SERVICE_URL = "https://localhost:9443/micro-gateway/v0.10/updated-apis";
     public static final String API_NAME = "name";
     public static final String API_SEQUENCE = "sequence";
     public static final String API_VERSION_PARAM = "{version}";
