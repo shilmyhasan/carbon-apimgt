aee304563cee3f2d16edc50db97365fae2d20f4c
uvindra <uvindra>
2019-01-28 16:50:30 +0530

    Fix for ECISUB-102

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/item-implement/js/jquery.endpointUi.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/item-implement/js/jquery.endpointUi.js
@@ -289,7 +289,7 @@
                     config.production_endpoints = config.production_endpoints[0];
                 if(config.sandbox_endpoints)
                     config.sandbox_endpoints = config.sandbox_endpoints[0];
-            }else{
+             } else if (config.endpoint_type === "load_balance" && config.algoCombo !== 'other') {
                 config.algoClassName = config.algoCombo ;                
             }
             return config;
