b021dc9c6d0c65b744043ecdec14df80f3dd5c1f
Maneesha Wijesekara <maneesha@Maneeshas-MacBook-Pro.local>
2019-02-01 17:35:13 +0530

    Improve audit logs with grouping attributes for applications

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConsumerImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConsumerImpl.java
@@ -2719,6 +2719,8 @@ public class APIConsumerImpl extends AbstractAPIManager implements APIConsumer {
         appLogObject.put(APIConstants.AuditLogConstants.NAME, application.getName());
         appLogObject.put(APIConstants.AuditLogConstants.TIER, application.getTier());
         appLogObject.put(APIConstants.AuditLogConstants.CALLBACK, application.getCallbackUrl());
+        appLogObject.put(APIConstants.AuditLogConstants.GROUPS, application.getGroupId());
+        appLogObject.put(APIConstants.AuditLogConstants.OWNER, application.getSubscriber().getName());
 
         APIUtil.logAuditMessage(APIConstants.AuditLogConstants.APPLICATION, appLogObject.toString(),
                 APIConstants.AuditLogConstants.CREATED, this.username);
@@ -2811,6 +2813,8 @@ public class APIConsumerImpl extends AbstractAPIManager implements APIConsumer {
         appLogObject.put(APIConstants.AuditLogConstants.TIER, application.getTier());
         appLogObject.put(APIConstants.AuditLogConstants.STATUS, existingApp != null ? existingApp.getStatus() : "");
         appLogObject.put(APIConstants.AuditLogConstants.CALLBACK, application.getCallbackUrl());
+        appLogObject.put(APIConstants.AuditLogConstants.GROUPS, application.getGroupId());
+        appLogObject.put(APIConstants.AuditLogConstants.OWNER, application.getSubscriber().getName());
 
         APIUtil.logAuditMessage(APIConstants.AuditLogConstants.APPLICATION, appLogObject.toString(),
                 APIConstants.AuditLogConstants.UPDATED, this.username);
@@ -2954,6 +2958,8 @@ public class APIConsumerImpl extends AbstractAPIManager implements APIConsumer {
             appLogObject.put(APIConstants.AuditLogConstants.NAME, application.getName());
             appLogObject.put(APIConstants.AuditLogConstants.TIER, application.getTier());
             appLogObject.put(APIConstants.AuditLogConstants.CALLBACK, application.getCallbackUrl());
+            appLogObject.put(APIConstants.AuditLogConstants.GROUPS, application.getGroupId());
+            appLogObject.put(APIConstants.AuditLogConstants.OWNER, application.getSubscriber().getName());
 
             APIUtil.logAuditMessage(APIConstants.AuditLogConstants.APPLICATION, appLogObject.toString(),
                     APIConstants.AuditLogConstants.DELETED, this.username);
