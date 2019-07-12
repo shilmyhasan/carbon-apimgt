aa86932992dcfcaeb77772e8fce824f3b1408671
selakap <selakap@wso2.com>
2019-07-12 19:44:19 +0530

    Fix for the Swagger in publisher and store behaves different

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/templates/api/swagger/swagger/swagger-ui.js
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/store/site/themes/wso2/templates/api/swagger/swagger/swagger-ui.js
@@ -22726,11 +22726,11 @@ SwaggerUi.partials.signature = (function () {
     var type = model.definition && model.definition.type ? model.definition.type : 'object';
     name = name || model.name;
 
-    if (config.modelsToIgnore.indexOf(type) > -1) {
+   if (config.modelsToIgnore.indexOf($ref) > -1) {
       type = 'loop';
-      name = modelType;
+      config.loopTo = modelType;
     } else {
-      config.modelsToIgnore.push(type);
+      config.modelsToIgnore.push($ref);
     }
 
     if (!model.definition) {
