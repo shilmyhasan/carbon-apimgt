6d94cf8e990ac21ef591928683b9cd0c3329dbe5
Dumindu-Kanchana <dkgeeganage@gmail.com>
2019-08-21 20:18:05 +0530

    fixing the error when creating version of an api

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ApisApiServiceImpl.java
@@ -211,7 +211,7 @@ public class ApisApiServiceImpl extends ApisApiService {
             }
 
             //Get all existing versions of  api been adding
-            List<String> apiVersions = apiProvider.getApiVersionsMatchingApiName(body.getName(), username);
+            List<String> apiVersions = apiProvider.getApiVersionsMatchingApiName(body.getName(), body.getProvider());
             if (apiVersions.size() > 0) {
                 //If any previous version exists
                 for (String version : apiVersions) {
