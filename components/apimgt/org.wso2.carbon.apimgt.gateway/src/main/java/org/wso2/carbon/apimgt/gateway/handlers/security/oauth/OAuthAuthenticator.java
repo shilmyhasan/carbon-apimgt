7e587f8d0f8faea3ad65e97f4fea4355b4673820
Ishara Cooray <isharac@wso2.com>
2020-02-20 12:31:16 +0530

    Revert "Add config to accept jwt token as a opaque token"
    This reverts commit 8fde2f433ce7b108542e9341079e8f3ea1e5a84a.

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/oauth/OAuthAuthenticator.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/oauth/OAuthAuthenticator.java
@@ -74,7 +74,6 @@ public class OAuthAuthenticator implements Authenticator {
     private String securityContextHeader;
     private boolean removeOAuthHeadersFromOutMessage=true;
     private boolean removeDefaultAPIHeaderFromOutMessage=true;
-    private boolean isJWTAnOpaqueToken = false;
     private String clientDomainHeader = "referer";
     private String requestOrigin;
     private String remainingAuthHeader;
@@ -186,7 +185,7 @@ public class OAuthAuthenticator implements Authenticator {
         String authenticationScheme;
         try {
             //Initial guess of a JWT token using the presence of a DOT.
-            if (!isJWTAnOpaqueToken && StringUtils.isNotEmpty(apiKey) && apiKey.contains(APIConstants.DOT)) {
+            if (StringUtils.isNotEmpty(apiKey) && apiKey.contains(APIConstants.DOT)) {
                 try {
                     // Check if the header part is decoded
                     Base64.getUrlDecoder().decode(apiKey.split("\\.")[0]);
@@ -487,10 +486,6 @@ public class OAuthAuthenticator implements Authenticator {
         String value = config.getFirstProperty(APIConstants.REMOVE_OAUTH_HEADERS_FROM_MESSAGE);
         if (value != null) {
             removeOAuthHeadersFromOutMessage = Boolean.parseBoolean(value);
-        }
-        value = config.getFirstProperty(APIConstants.JWT_AS_OPAQUE_TOKEN);
-        if (value != null) {
-            isJWTAnOpaqueToken = Boolean.parseBoolean(value);
         }
         value = config.getFirstProperty(APIConstants.JWT_HEADER);
         if (value != null) {
