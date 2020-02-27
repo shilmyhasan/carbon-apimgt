8daeee781fe505b2129e1b1b8107a4d65ace9530
dushaniw <dushaniwellappili@gmail.com>
2020-02-27 18:56:02 +0530

    Fix https://github.com/wso2/product-apim/issues/6629

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/Resources/components/operationComponents/AddParameter.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/Resources/components/operationComponents/AddParameter.jsx
@@ -210,6 +210,7 @@ function AddParameter(props) {
                             size='small'
                             variant='outlined'
                             aria-label='add'
+                            color='primary'
                             onClick={addNewParameter}
                         >
                             Add
