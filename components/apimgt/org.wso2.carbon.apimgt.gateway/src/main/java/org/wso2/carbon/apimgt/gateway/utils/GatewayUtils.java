213b66e352c35d625d5b99d84f84104bda05f233
Sarangan0219 <sarangan@wso2.com>
2020-04-16 16:05:46 +0530

    remove newlines

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/utils/GatewayUtils.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/utils/GatewayUtils.java
@@ -1,21 +1,21 @@
 /*
- *  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- *
- *  WSO2 Inc. licenses this file to you under the Apache License,
- *  Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing,
- *  software distributed under the License is distributed on an
- *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- *  KIND, either express or implied.  See the License for the
- *  specific language governing permissions and limitations
- *  under the License.
- *
- */
+*  Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing,
+*  software distributed under the License is distributed on an
+*  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+*  KIND, either express or implied.  See the License for the
+*  specific language governing permissions and limitations
+*  under the License.
+*
+*/
 package org.wso2.carbon.apimgt.gateway.utils;
 
 import org.apache.axis2.clustering.ClusteringAgent;
@@ -357,12 +357,10 @@ public class GatewayUtils {
                     byteArrayOutputStream.write(buffer, 0, length);
                 }
                 byteArrayOutputStream.flush();
-
                 inputStreamSchema = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
                 inputStreamXml = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
                 inputStreamOriginal = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
                 inputStreamJSON = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
-
             } else {
                 String payload;
                 if ( ThreatProtectorConstants.APPLICATION_JSON.equals(contentType)){
@@ -372,19 +370,16 @@ public class GatewayUtils {
                 } else {
                    payload = axis2MC.getEnvelope().getBody().getFirstElement().toString();
                 }
-
                 inputStreamSchema = new ByteArrayInputStream(payload.getBytes(StandardCharsets.UTF_8));
                 inputStreamXml= new ByteArrayInputStream(payload.getBytes(StandardCharsets.UTF_8));
                 inputStreamOriginal= new ByteArrayInputStream(payload.getBytes(StandardCharsets.UTF_8));
                 inputStreamJSON= new ByteArrayInputStream(payload.getBytes(StandardCharsets.UTF_8));
             }
         }
-
         inputStreamMap.put(ThreatProtectorConstants.SCHEMA, inputStreamSchema);
         inputStreamMap.put(ThreatProtectorConstants.XML, inputStreamXml);
         inputStreamMap.put(ThreatProtectorConstants.ORIGINAL, inputStreamOriginal);
         inputStreamMap.put(ThreatProtectorConstants.JSON, inputStreamJSON);
-
         return inputStreamMap;
     }
 
