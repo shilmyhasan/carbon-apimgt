131022fa942d53c013322c404de218a145318320
msm1992 <madusha.sachini@yahoo.com>
2020-04-21 16:09:34 +0530

    Add review changes

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -12309,7 +12309,12 @@ public class ApiMgtDAO {
     }
 
     public boolean isPolicyExist(String policyType, int tenantId, String policyName) throws APIManagementException {
-        return isPolicyExist(null, policyType, tenantId, policyName);
+        try (Connection connection = APIMgtDBUtil.getConnection()) {
+            return isPolicyExist(connection, policyType, tenantId, policyName);
+        } catch (SQLException e) {
+            handleException("Error while checking policy existence " + policyName + "-" + tenantId, e);
+        }
+        return false;
     }
 
     public boolean isPolicyExist(Connection connection, String policyType, int tenantId, String policyName)
@@ -12330,9 +12335,6 @@ public class ApiMgtDAO {
         try {
             String query = "SELECT " + PolicyConstants.POLICY_ID + " FROM " + policyTable
                     + " WHERE TENANT_ID =? AND NAME = ? ";
-            if (connection == null) {
-                connection = APIMgtDBUtil.getConnection();
-            }
             connection.setAutoCommit(true);
             isExistStatement = connection.prepareStatement(query);
             isExistStatement.setInt(1, tenantId);
