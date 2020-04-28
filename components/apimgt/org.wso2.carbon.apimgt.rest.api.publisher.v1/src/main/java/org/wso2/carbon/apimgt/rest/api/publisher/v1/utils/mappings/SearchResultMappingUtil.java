d4d1a42a41e0b8a9546dca66e7b5bb31806e3104
wasuradananjith <wasuradananjith@gmail.com>
2020-04-28 17:44:52 +0530

    Modify REST API to retrieve API Product version

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/utils/mappings/SearchResultMappingUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/utils/mappings/SearchResultMappingUtil.java
@@ -80,6 +80,7 @@ public class SearchResultMappingUtil {
         apiProductResultDTO.setId(apiProduct.getUuid());
         APIProductIdentifier apiproductId = apiProduct.getId();
         apiProductResultDTO.setName(apiproductId.getName());
+        apiProductResultDTO.setVersion(apiproductId.getVersion());
         apiProductResultDTO.setProvider(apiproductId.getProviderName());
         String context = apiProduct.getContextTemplate();
         if (context.endsWith("/" + RestApiConstants.API_VERSION_PARAM)) {
