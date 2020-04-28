ac3902599589dd021b20d5f318e975585300a50d
Saranki Magenthirarajah <m.saranki@gmail.com>
2020-04-28 11:39:39 +0530

    Removed the commented lines

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/SubscriptionsApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/SubscriptionsApiServiceImpl.java
@@ -320,7 +320,6 @@ public class SubscriptionsApiServiceImpl implements SubscriptionsApiService {
                 RestAPIStoreUtils.checkSubscriptionAllowed(apiTypeWrapper,
                         subscriptionDTO.getThrottlingPolicy());
 
-                //apiIdentifier.setTier(subscriptionDTO.getThrottlingPolicy());
                 apiTypeWrapper.setTier(subscriptionDTO.getThrottlingPolicy());
                 SubscriptionResponse subscriptionResponse = apiConsumer
                         .addSubscription(apiTypeWrapper, username, application.getId());
