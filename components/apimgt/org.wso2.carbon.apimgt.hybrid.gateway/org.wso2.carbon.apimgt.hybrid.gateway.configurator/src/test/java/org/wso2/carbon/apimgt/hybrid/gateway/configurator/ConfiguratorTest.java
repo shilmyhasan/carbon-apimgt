92f3331dc7068a067745be5be05fcc2e6b24c25f
erandiganepola <erandiganepola@gmail.com>
2020-05-05 14:46:39 +0530

    Fix indentation according to the PR review comments

--- components/apimgt/org.wso2.carbon.apimgt.hybrid.gateway/org.wso2.carbon.apimgt.hybrid.gateway.configurator/src/test/java/org/wso2/carbon/apimgt/hybrid/gateway/configurator/ConfiguratorTest.java
+++ components/apimgt/org.wso2.carbon.apimgt.hybrid.gateway/org.wso2.carbon.apimgt.hybrid.gateway.configurator/src/test/java/org/wso2/carbon/apimgt/hybrid/gateway/configurator/ConfiguratorTest.java
@@ -55,7 +55,7 @@ public class ConfiguratorTest {
     private String carbonConfigDirPath;
     private String gatewayConfigPath;
     private String deploymentTomlFilePath;
-    private String args[] = { "test-email@test.com", "TestTenant", "TestPassword" };
+    private String args[] = {"test-email@test.com", "TestTenant", "TestPassword"};
     private static final String TOKEN = "$token";
 
     @Before
@@ -63,12 +63,14 @@ public class ConfiguratorTest {
         TestUtil util = new TestUtil();
         util.setupCarbonHome();
         String carbonHome = System.getProperty(ConfigConstants.CARBON_HOME);
-        carbonConfigDirPath = carbonHome + File.separator + ConfigConstants.REPOSITORY_DIR + File.separator
+        carbonConfigDirPath =
+            carbonHome + File.separator + ConfigConstants.REPOSITORY_DIR + File.separator
                 + ConfigConstants.CONF_DIR;
-        gatewayConfigPath = carbonConfigDirPath + File.separator + OnPremiseGatewayConstants.CONFIG_FILE_TOML_NAME;
+        gatewayConfigPath =
+            carbonConfigDirPath + File.separator + OnPremiseGatewayConstants.CONFIG_FILE_TOML_NAME;
         gatewayConfigs = Configurator.getGatewayConfigs(gatewayConfigPath, args);
         String configToolPropertyFilePath = carbonConfigDirPath + File.separator +
-                ConfigConstants.CONFIG_TOOL_CONFIG_FILE_NAME;
+            ConfigConstants.CONFIG_TOOL_CONFIG_FILE_NAME;
         configToolProperties = Configurator.readPropertiesFromFile(configToolPropertyFilePath);
         deploymentTomlFilePath = carbonConfigDirPath + File.separator +
             ConfigConstants.DEPLOYMENT_TOML_FILE_NAME;
@@ -76,17 +78,18 @@ public class ConfiguratorTest {
 
     @Test
     public void setAPIMConfigurations() throws OnPremiseGatewayException {
-      Toml parsed = new Toml().read(new File(deploymentTomlFilePath));
-      Map<String, Object> tomlEntries = parsed.toMap();
-      DeploymentTomlConfigurator.setDeploymentTomlConfigurations(deploymentTomlFilePath,
-          configToolProperties, tomlEntries, gatewayConfigs);
+        Toml parsed = new Toml().read(new File(deploymentTomlFilePath));
+        Map<String, Object> tomlEntries = parsed.toMap();
+        DeploymentTomlConfigurator.setDeploymentTomlConfigurations(deploymentTomlFilePath,
+            configToolProperties, tomlEntries, gatewayConfigs);
     }
 
     @Test
     public void initializeGateway() throws Exception {
         //Collect device details
         Map<String, String> deviceDetails = Configurator.getDeviceDetails();
-        String carbonFilePath = carbonConfigDirPath + File.separator + ConfigConstants.GATEWAY_CARBON_FILE_NAME;
+        String carbonFilePath =
+            carbonConfigDirPath + File.separator + ConfigConstants.GATEWAY_CARBON_FILE_NAME;
         int port = Configurator.getGatewayPort(carbonFilePath);
         deviceDetails.put(ConfigConstants.PORT, Integer.toString(port));
         String payload = Configurator.getInitializationPayload(gatewayConfigs, deviceDetails, args);
@@ -94,7 +97,8 @@ public class ConfiguratorTest {
         //Set initialization endpoint
         String initApiUrl = gatewayConfigs.getUrl_initialization_api();
         //Update details in gateway properties file
-        String gatewayConfigPath = carbonConfigDirPath + File.separator + OnPremiseGatewayConstants.CONFIG_FILE_TOML_NAME;
+        String gatewayConfigPath =
+            carbonConfigDirPath + File.separator + OnPremiseGatewayConstants.CONFIG_FILE_TOML_NAME;
         Configurator.updateOnPremGatewayUniqueId(gatewayConfigPath, TOKEN);
     }
 
@@ -103,27 +107,32 @@ public class ConfiguratorTest {
         String carbonHome = System.getProperty(ConfigConstants.CARBON_HOME);
         Toml parsed = new Toml().read(new File(deploymentTomlFilePath));
         Map<String, Object> tomlEntries = parsed.toMap();
-        DeploymentTomlConfigurator.setDeploymentTomlConfigurations(deploymentTomlFilePath,
-            configToolProperties, tomlEntries, gatewayConfigs);
+        DeploymentTomlConfigurator
+            .setDeploymentTomlConfigurations(deploymentTomlFilePath, configToolProperties,
+                tomlEntries, gatewayConfigs);
         RegistryXmlConfigurator registryXmlConfigurator = new RegistryXmlConfigurator();
-        TransformerIdentityImpl transformerIdentity = PowerMockito.mock(TransformerIdentityImpl.class);
+        TransformerIdentityImpl transformerIdentity = PowerMockito
+            .mock(TransformerIdentityImpl.class);
         TransformerFactory transformerFactory = PowerMockito.mock(TransformerFactory.class);
         PowerMockito.mockStatic(TransformerFactory.class);
         PowerMockito.when(TransformerFactory.newInstance()).thenReturn(transformerFactory);
         PowerMockito.when(transformerFactory.newTransformer()).thenReturn(transformerIdentity);
-        PowerMockito.doNothing().when(transformerIdentity).transform(any(DOMSource.class), any(StreamResult.class));
+        PowerMockito.doNothing().when(transformerIdentity)
+            .transform(any(DOMSource.class), any(StreamResult.class));
         registryXmlConfigurator.configure(carbonConfigDirPath, gatewayConfigs);
         Log4JConfigurator log4JConfigurator = new Log4JConfigurator();
         log4JConfigurator.configure(carbonConfigDirPath);
         Configurator.writeConfiguredLock(carbonHome);
         //Cleaning the log4j.properties file
-        PrintWriter writer = new PrintWriter(carbonHome + File.separator + ConfigConstants.REPOSITORY_DIR + File.separator + ConfigConstants.CONF_DIR + File.separator + "log4j.properties");
+        PrintWriter writer = new PrintWriter(
+            carbonHome + File.separator + ConfigConstants.REPOSITORY_DIR + File.separator
+                + ConfigConstants.CONF_DIR + File.separator + "log4j.properties");
         writer.print("\n");
         writer.close();
     }
 
     @After
-    public void tearDown () throws IOException {
+    public void tearDown() throws IOException {
         String deploymentTomlBackup = deploymentTomlFilePath + ".backup";
         if (Files.exists(Paths.get(deploymentTomlFilePath)) &&
             Files.exists(Paths.get(deploymentTomlBackup))) {
