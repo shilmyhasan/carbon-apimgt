7942da35147dc289dbc1c4aa76f8c11a47f175fb
Chanaka Jayasena <chanaka3d@gmail.com>
2020-06-03 09:10:47 +0530

    Adding support to disable one of the Grid or List view from the API Listing pages

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Listing/TableView/TableView.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Listing/TableView/TableView.jsx
@@ -62,11 +62,19 @@ const styles = (theme) => ({
 class TableView extends React.Component {
     constructor(props) {
         super(props);
+        let { defaultApiView } = props.theme.custom;
+        this.showToggle = true;
+        if (typeof defaultApiView === 'object' && defaultApiView.length > 0) {
+            if (defaultApiView.length === 1) { // We will disable toggle buttons
+                this.showToggle = false;
+            }
+            defaultApiView = defaultApiView[defaultApiView.length - 1];
+        }
         this.state = {
             apisAndApiProducts: null,
             notFound: true,
             displayCount: 0,
-            listType: props.theme.custom.defaultApiView,
+            listType: defaultApiView,
         };
         this.page = 0;
         this.count = 100;
@@ -443,6 +451,7 @@ class TableView extends React.Component {
                         setListType={this.setListType}
                         isAPIProduct={isAPIProduct}
                         listType={listType}
+                        showToggle={this.showToggle}
                     />
                     <div className={classes.contentInside}>
                         {isAPIProduct ? (
@@ -463,6 +472,7 @@ class TableView extends React.Component {
                     setListType={this.setListType}
                     isAPIProduct={isAPIProduct}
                     listType={listType}
+                    showToggle={this.showToggle}
                     query={query}
                 />
                 <div className={classes.contentInside}>
