82833763fcbbfa39785b0266138c942e98952060
msm1992 <madusha.sachini@yahoo.com>
2020-06-11 14:12:17 +0530

    Backport Scope Mapping Feature

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
@@ -3976,10 +3976,7 @@ public final class APIUtil {
             if (log.isDebugEnabled()) {
                 log.debug("Adding/updating tenant-conf.json to the registry of tenant " + tenantID);
             }
-            Resource resource = registry.newResource();
-            resource.setMediaType(APIConstants.APPLICATION_JSON_MEDIA_TYPE);
-            resource.setContent(data);
-            registry.put(APIConstants.API_TENANT_CONF_LOCATION, resource);
+            updateTenantConf(registry, data);
             if (log.isDebugEnabled()) {
                 log.debug("Successfully added/updated tenant-conf.json of tenant  " + tenantID);
             }
@@ -3988,6 +3985,25 @@ public final class APIUtil {
         } catch (IOException e) {
             throw new APIManagementException("Error while reading tenant conf file content of tenant " + tenantID, e);
         }
+    }
+
+    public static void updateTenantConf(String tenantConfString, String tenantDomain) throws APIManagementException {
+        RegistryService registryService = ServiceReferenceHolder.getInstance().getRegistryService();
+        int tenantId = getTenantIdFromTenantDomain(tenantDomain);
+        try {
+            UserRegistry registry = registryService.getConfigSystemRegistry(tenantId);
+            updateTenantConf(registry, tenantConfString.getBytes());
+        } catch (RegistryException e) {
+            throw new APIManagementException("Error while saving tenant conf to the registry of tenant "
+                    + tenantDomain, e);
+        }
+    }
+
+    private static void updateTenantConf(UserRegistry registry, byte[] data) throws RegistryException {
+        Resource resource = registry.newResource();
+        resource.setMediaType(APIConstants.APPLICATION_JSON_MEDIA_TYPE);
+        resource.setContent(data);
+        registry.put(APIConstants.API_TENANT_CONF_LOCATION, resource);
     }
 
     /**
@@ -4006,10 +4022,7 @@ public final class APIUtil {
             }
             byte[] data = getLocalTenantConfFileData();
             log.debug("Adding tenant config to the registry");
-            Resource resource = registry.newResource();
-            resource.setMediaType(APIConstants.APPLICATION_JSON_MEDIA_TYPE);
-            resource.setContent(data);
-            registry.put(APIConstants.API_TENANT_CONF_LOCATION, resource);
+            updateTenantConf(registry, data);
         } catch (RegistryException e) {
             throw new APIManagementException("Error while saving tenant conf to the registry", e);
         } catch (IOException e) {
@@ -7036,6 +7049,11 @@ public final class APIUtil {
             }
         }
         return restAPIConfigJSON;
+    }
+
+    public static JSONObject getTenantConfig(String tenantDomain) throws APIManagementException {
+        int tenantId = getTenantIdFromTenantDomain(tenantDomain);
+        return getTenantConfig(tenantId);
     }
 
     /**
