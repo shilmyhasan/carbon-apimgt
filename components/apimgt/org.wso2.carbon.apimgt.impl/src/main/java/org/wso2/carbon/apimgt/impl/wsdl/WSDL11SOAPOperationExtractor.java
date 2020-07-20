210c944014b65188602e2cb0a649858fc779c5e6
Hasunie <hasunie@wso2.com>
2020-07-20 17:24:25 +0530

    fix formatting issue

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/wsdl/WSDL11SOAPOperationExtractor.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/wsdl/WSDL11SOAPOperationExtractor.java
@@ -159,7 +159,7 @@ public class WSDL11SOAPOperationExtractor extends WSDL11ProcessorImpl {
     @Override
     public void loadXSDs(APIMWSDLReader wsdlReader, String url) throws APIManagementException {
         Collection<File> foundXSDFiles = new java.util.LinkedList<>();
-        if (url!= null && url.endsWith(File.pathSeparator + "extracted")) {
+        if (url != null && url.endsWith(File.pathSeparator + "extracted")) {
             File folderToImport = new File(url);
             foundXSDFiles = APIFileUtil.searchFilesWithMatchingExtension(folderToImport, "xsd",
                     false);
