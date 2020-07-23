2f972bc4b2c68d3ce7e5ebb5e1ab062cd76d1eaf
kavishkafernando <kavishkafdo95@gmail.com>
2020-07-23 12:31:56 +0530

    Fixes #5566 and #8782

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/DevPortal.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/DevPortal.jsx
@@ -70,6 +70,14 @@ class DevPortal extends React.Component {
      *  Mounting the components
      */
     componentDidMount() {
+        const { app: { customUrl: { tenantDomain: customUrlEnabledDomain } } } = Settings;
+        let tenant = null;
+        const urlParams = new URLSearchParams(window.location.search);
+        if (customUrlEnabledDomain !== 'null') {
+            tenant = customUrlEnabledDomain;
+        } else {
+            tenant = urlParams.get('tenant')
+        }
         const api = new API();
         const promisedSettings = api.getSettings();
         promisedSettings
@@ -81,8 +89,7 @@ class DevPortal extends React.Component {
                     error,
                 );
             });
-        const urlParams = new URLSearchParams(window.location.search);
-        if (urlParams.get('tenant') === null || urlParams.get('tenant') === 'carbon.super') {
+        if (tenant === null || tenant === 'carbon.super') {
             const { custom: { publicTenantStore } } = this.systemTheme;
             if(publicTenantStore) {
                 const { active: publicTenantStoreActive, redirectToIfInactive } = publicTenantStore;
@@ -98,7 +105,7 @@ class DevPortal extends React.Component {
                 this.setState({ theme: this.systemTheme, redirecting: false });
             }
         } else {
-            this.setTenantTheme(urlParams.get('tenant'));
+            this.setTenantTheme(tenant);
         }
 
         
