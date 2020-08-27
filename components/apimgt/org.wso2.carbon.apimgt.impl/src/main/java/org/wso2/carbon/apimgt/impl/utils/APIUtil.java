b2814dfb99af4bab8d803526c2985260ed5db66a
chashikajw <chashikajw007@gmail.com>
2020-08-27 11:24:22 +0530

    Remove port value from IP address in X-Forwarded-For header

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/utils/APIUtil.java
@@ -210,6 +210,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.net.Inet4Address;
 import java.net.InetAddress;
@@ -217,6 +218,7 @@ import java.net.MalformedURLException;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.URL;
+import java.net.UnknownHostException;
 import java.nio.charset.Charset;
 import java.rmi.RemoteException;
 import java.security.InvalidKeyException;
@@ -8053,6 +8055,31 @@ public final class APIUtil {
 
         }
         return result;
+    }
+
+    /**
+     * This method provides the BigInteger value for the given IP address. This supports both IPv4 and IPv6 address
+     *
+     * @param ipAddress ip address
+     * @return BigInteger value for the given ip address. returns 0 for unknown host
+     */
+    public static BigInteger ipToBigInteger(String ipAddress) {
+
+        InetAddress address;
+        try {
+            address = getAddress(ipAddress);
+            byte[] bytes = address.getAddress();
+            return new BigInteger(1, bytes);
+        } catch (UnknownHostException e) {
+            //ignore the error and log it
+            log.error("Error while parsing host IP " + ipAddress, e);
+        }
+        return BigInteger.ZERO;
+    }
+
+    public static InetAddress getAddress(String ipAddress) throws UnknownHostException {
+
+        return InetAddress.getByName(ipAddress);
     }
 
     public String getFullLifeCycleData(Registry registry) throws XMLStreamException, RegistryException {
