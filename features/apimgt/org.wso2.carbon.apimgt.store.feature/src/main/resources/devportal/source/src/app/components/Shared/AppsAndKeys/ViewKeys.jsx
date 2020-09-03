2220b8611a335a9552dd858265b9d5763c6ecf1c
Vithursa <vithu30@hotmail.com>
2020-09-03 14:35:20 +0530

    Set token validity period in UI

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Shared/AppsAndKeys/ViewKeys.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Shared/AppsAndKeys/ViewKeys.jsx
@@ -36,6 +36,7 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import { FormattedMessage, injectIntl } from 'react-intl';
+import Settings from 'AppComponents/Shared/SettingsContext';
 import ResourceNotFound from '../../Base/Errors/ResourceNotFound';
 import Loading from '../../Base/Loading/Loading';
 import Application from '../../../data/Application';
@@ -86,6 +87,7 @@ const styles = theme => ({
  * Class used to displays in key generation UI
  */
 class ViewKeys extends React.Component {
+    static contextType = Settings;
     /**
      * @param {*} props properties
      */
@@ -117,11 +119,14 @@ class ViewKeys extends React.Component {
      */
     componentDidMount() {
         const { accessTokenRequest } = this.state;
+        const settingsContext = this.context;
+        const { appAccessTokenValidity } = settingsContext.settings;
         const { keyType } = this.props;
         this.applicationPromise
             .then((application) => {
                 application.getKeys().then(() => {
                     const newRequest = { ...accessTokenRequest, keyType };
+                    newRequest.timeout = appAccessTokenValidity;
                     const subscriptionScopes = application.subscriptionScopes
                         .map((scope) => { return scope.scopeKey; });
                     this.setState({ accessTokenRequest: newRequest, subscriptionScopes });
