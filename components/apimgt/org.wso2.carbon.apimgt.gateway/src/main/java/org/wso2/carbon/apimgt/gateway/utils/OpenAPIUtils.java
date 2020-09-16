fdd2eb6eafce0df9560236495f6f41ae76fdbc9c
dushansilva <dushansilva95@gmail.com>
2020-09-16 16:20:35 +0530

    fix scope issue in trailing slash

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/utils/OpenAPIUtils.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/utils/OpenAPIUtils.java
@@ -205,7 +205,10 @@ public class OpenAPIUtils {
                     ((Axis2MessageContext) synCtx).getAxis2MessageContext();
             String httpMethod = (String) axis2MessageContext.getProperty(APIConstants.DigestAuthConstants.HTTP_METHOD);
             PathItem path = openAPI.getPaths().get(apiElectedResource);
-
+            if (path == null) {
+                String apiElectedResourceWithSlash = (String) synCtx.getProperty("API_ELECTED_RESOURCE_WITH_SLASH");
+                path = openAPI.getPaths().get(apiElectedResourceWithSlash);
+            }
             if (path != null) {
                 switch (httpMethod) {
                     case APIConstants.HTTP_GET:
@@ -235,6 +238,10 @@ public class OpenAPIUtils {
                     ((Axis2MessageContext) synCtx).getAxis2MessageContext();
             String httpMethod = (String) axis2MessageContext.getProperty(APIConstants.DigestAuthConstants.HTTP_METHOD);
             PathItem path = openAPI.getPaths().get(apiElectedResource);
+            if (path == null) {
+                String apiElectedResourceWithSlash = (String) synCtx.getProperty("API_ELECTED_RESOURCE_WITH_SLASH");
+                path = openAPI.getPaths().get(apiElectedResourceWithSlash);
+            }
 
             if (path != null) {
                 Operation operation = path.readOperationsMap().get(PathItem.HttpMethod.valueOf(httpMethod));
