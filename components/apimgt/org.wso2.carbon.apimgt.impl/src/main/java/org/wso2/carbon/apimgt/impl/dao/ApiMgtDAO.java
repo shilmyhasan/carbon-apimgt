204fb0c6c51e02424a749fee7b263e984db62392
chamilaadhi <cka.adhi@gmail.com>
2020-10-16 16:42:48 +0530

    Refactor

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/ApiMgtDAO.java
@@ -4716,9 +4716,8 @@ public class ApiMgtDAO {
 
         try {
             connection = APIMgtDBUtil.getConnection();
-            if (connection.getMetaData().getDriverName().contains("MS SQL")
-                    || connection.getMetaData().getDriverName().contains("Microsoft")
-                    || connection.getMetaData().getDriverName().contains("Oracle")) {
+            String driverName = connection.getMetaData().getDriverName();
+            if (driverName.contains("MS SQL") || driverName.contains("Microsoft") || driverName.contains("Oracle")) {
                 offset = start + offset;
             }
             // sortColumn, sortOrder variable values has sanitized in jaggery level (applications-list.jag)for security.
@@ -4728,7 +4727,7 @@ public class ApiMgtDAO {
             } else {
                 sqlQuery = sqlQuery.replace("$2", "asc");
             }
-            if (connection.getMetaData().getDriverName().contains("Oracle") && "CREATED_BY".equals(sortColumn)) {
+            if (driverName.contains("Oracle") && "CREATED_BY".equals(sortColumn)) {
                 sqlQuery = sqlQuery.replace("$3", "APP.CREATED_BY");
             } else {
                 sqlQuery = sqlQuery.replace("$3", sortColumn);
