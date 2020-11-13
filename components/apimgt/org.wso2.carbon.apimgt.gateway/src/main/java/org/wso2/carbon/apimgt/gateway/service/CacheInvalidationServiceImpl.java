abac2aaef502ad39ce62875bb59ccc4bad8d654a
dinusha92 <dinushad92@gmail.com>
2020-11-13 16:00:55 +0530

    Fixes token invalidation upon new subscription of APIs.

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/service/CacheInvalidationServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/service/CacheInvalidationServiceImpl.java
@@ -168,14 +168,12 @@ public class CacheInvalidationServiceImpl implements CacheInvalidationService {
         try {
             isSuperTenantFlowStarted = startTenantFlow(MultitenantConstants.SUPER_TENANT_DOMAIN_NAME);
             gatewayCache = CacheProvider.getGatewayTokenCache();
-            Cache invalidGatewayCache = CacheProvider.getInvalidTokenCache();
             for (String accessToken : accessTokens) {
                 Object cacheEntry = gatewayCache.get(accessToken);
                 if (cacheEntry != null) {
                     //cachePut(accessToken, tenantDomain)
                     cachedObjects.put(accessToken, cacheEntry.toString());
                     gatewayCache.remove(accessToken);
-                    invalidGatewayCache.put(accessToken, cacheEntry.toString());
                 }
             }
         } finally {
@@ -231,14 +229,12 @@ public class CacheInvalidationServiceImpl implements CacheInvalidationService {
                 }
 
                 Cache tenantGatewayCache = CacheProvider.getGatewayTokenCache();
-                Cache invalidtTenantGatewayCache = CacheProvider.getInvalidTokenCache();
 
                 //Remove all cached tokens from the tenant's cache
                 //Note: Best solution would have been to use the removeAll method of the cache. But it currently throws
                 //an NPE if at least one key in the list doesn't exist in the cache.
                 for (String accessToken : tenantMap.get(tenantDomain)) {
                     tenantGatewayCache.remove(accessToken);
-                    invalidtTenantGatewayCache.put(accessToken, tenantDomain);
                 }
 
                 if (log.isDebugEnabled()) {
