c7b584b30337f6230d90f3dd3c1fc2514cee8cb3
Shalki <swenushika@gmail.com>
2020-11-17 22:19:42 +0530

    Correct formatting issues

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
@@ -317,7 +317,7 @@ public class ApisApiServiceImpl implements ApisApiService {
         String username = RestApiUtil.getLoggedInUsername();
         List<String> apiSecuritySchemes = body.getSecurityScheme();//todo check list vs string
         String context = "/" +  body.getContext() + "/" + body.getVersion();
-        System.out.println(context);
+
 
         if (!apiProvider.isClientCertificateBasedAuthenticationConfigured() && apiSecuritySchemes != null) {
             for (String apiSecurityScheme : apiSecuritySchemes) {
