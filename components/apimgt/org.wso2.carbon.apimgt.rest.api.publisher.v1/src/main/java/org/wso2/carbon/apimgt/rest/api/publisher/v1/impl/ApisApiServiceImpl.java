2e591e22cc73e56658cacb14eadd09325d510356
Shalki <swenushika@gmail.com>
2020-11-17 22:24:30 +0530

    Correct formatting issues

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
@@ -318,7 +318,6 @@ public class ApisApiServiceImpl implements ApisApiService {
         List<String> apiSecuritySchemes = body.getSecurityScheme();//todo check list vs string
         String context = "/" +  body.getContext() + "/" + body.getVersion();
 
-
         if (!apiProvider.isClientCertificateBasedAuthenticationConfigured() && apiSecuritySchemes != null) {
             for (String apiSecurityScheme : apiSecuritySchemes) {
                 if (apiSecurityScheme.contains(APIConstants.API_SECURITY_MUTUAL_SSL)) {
@@ -356,7 +355,6 @@ public class ApisApiServiceImpl implements ApisApiService {
             RestApiUtil.handleBadRequest("Error occurred while adding API. API with name " + body.getName()
                     + " already exists.", log);
         }
-
 
         if (body.getAuthorizationHeader() == null) {
             body.setAuthorizationHeader(APIUtil
