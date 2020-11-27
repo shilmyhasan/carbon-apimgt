54b03fc66a3465c3f46cde597d43259fa75182fa
npamudika <rpnpamudika@gmail.com>
2020-11-27 14:32:27 +0530

    Correct licence headers

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/common/TransportHeaderHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/common/TransportHeaderHandler.java
@@ -1,17 +1,19 @@
 /*
- *  Copyright WSO2 Inc.
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.wso2.carbon.apimgt.gateway.handlers.common;
 
@@ -96,7 +98,7 @@ public class TransportHeaderHandler extends AbstractSynapseHandler {
         log.debug("Removing headers completed in response out flow");
 
         if (this.isRemoveRequestHeadersOnFaultEnabled) {
-            //Removes all the headers present in the request if the request has not reached the backend.
+            //Remove all the headers present in the request if the request has not reached the backend.
             NHttpConnection sourceHttpConnection =
                     (NHttpConnection)((Axis2MessageContext)synCtx).getAxis2MessageContext().
                             getProperty(TransportHeaderUtil.PASSTHROUGH_SOURCE_CONNECTION);
