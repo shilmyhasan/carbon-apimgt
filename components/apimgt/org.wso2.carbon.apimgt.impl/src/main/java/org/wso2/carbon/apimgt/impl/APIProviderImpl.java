118f294223ce1f14e1c1f7933eedd8f7bb1801fc
wasuradananjith <wasuradananjith@gmail.com>
2021-01-25 07:18:31 +0530

    Handle bad request in getResourcesToBeRemovedFromAPIProducts

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -8047,7 +8047,7 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
             uriTemplates = oasParser.getURITemplates(apiDefinition);
         } catch (APIManagementException e) {
             // catch APIManagementException inside again to capture validation error
-            log.error("Swagger validation error");
+            RestApiUtil.handleBadRequest(e.getMessage(), log);
         }
         if(uriTemplates == null || uriTemplates.isEmpty()) {
             log.error("No resources found");
