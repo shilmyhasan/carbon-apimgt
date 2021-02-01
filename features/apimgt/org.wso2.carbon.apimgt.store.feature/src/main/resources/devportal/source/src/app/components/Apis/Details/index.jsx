5edd93e92ba474c903c8c1b682ca1751e226c4cd
Chamindu36 <cudakara@gmail.com>
2021-02-01 13:59:54 +0530

    Fixing the error of application limit in the UI devportal

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Apis/Details/index.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Apis/Details/index.jsx
@@ -217,7 +217,8 @@ class Details extends React.Component {
             const user = AuthManager.getUser();
             if (user != null) {
                 existingSubscriptions = restApi.getSubscriptions(this.api_uuid, null);
-                promisedApplications = restApi.getAllApplications();
+                const subscriptionLimit = Settings.app.subscribeApplicationLimit || 5000;
+                promisedApplications = restApi.getAllApplications(null, subscriptionLimit);
 
                 Promise.all([existingSubscriptions, promisedApplications])
                     .then((response) => {
@@ -233,7 +234,7 @@ class Details extends React.Component {
                                 policy: element.throttlingPolicy,
                                 status: element.status,
                                 subscriptionId: element.subscriptionId,
-                                label: appIdToNameMapping[element.applicationId],
+                                label: element.applicationInfo.name,
                             };
                         });
 
