4f8fc1bafe743f5380d810418e246817d222c2ca
msm1992 <madusha.sachini@yahoo.com>
2021-02-02 08:18:09 +0530

    Fixes product-apim #9667

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/item-design/js/api-design.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/item-design/js/api-design.js
@@ -272,10 +272,16 @@ function APIDesigner(){
 
 APIDesigner.prototype.remove_trailing_slash = function(swagger) {
     var paths = swagger.paths;
+    var designer = APIDesigner();
+
     for (var path in paths) {
         if (path.length > 1 && path.endsWith("/")) {
             var newkey = path.slice(0, -1);
-            swagger.paths[newkey] = swagger.paths[path];
+            if (designer.check_if_resource_path_exist(newkey)) {
+                Object.assign(swagger.paths[newkey], swagger.paths[path]);
+            } else {
+                swagger.paths[newkey] = swagger.paths[path];
+            }
             delete swagger.paths[path];
         }
     }
@@ -304,6 +310,27 @@ APIDesigner.prototype.check_if_resource_exist = function(path, method){
 
                 return true;
             }
+        }
+    }
+    return false;
+}
+
+APIDesigner.prototype.check_if_resource_path_exist = function(path){
+
+    for (var key in this.api_doc.paths) {
+
+        //remove tailing slash
+        if (path.lastIndexOf('/') == path.length -1) {
+            path = path.substring(0, path.length -1);
+        }
+
+        var keyWithoutTailingSlash = key;
+        if (key.lastIndexOf('/') == key.length -1) {
+            keyWithoutTailingSlash = key.substring(0, key.length -1);
+        }
+
+        if(keyWithoutTailingSlash.toLowerCase() == path.toLowerCase()){
+            return true;
         }
     }
     return false;
