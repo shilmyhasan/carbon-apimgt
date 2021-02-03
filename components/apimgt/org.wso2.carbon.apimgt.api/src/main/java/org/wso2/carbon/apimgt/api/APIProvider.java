4d02669b365c22ecc7d0b66c6b9812642a4fdd16
tharikaGitHub <tharika.m92@gmail.com>
2021-02-03 22:26:19 +0530

    Fix conflicts

--- components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/APIProvider.java
+++ components/apimgt/org.wso2.carbon.apimgt.api/src/main/java/org/wso2/carbon/apimgt/api/APIProvider.java
@@ -829,9 +829,6 @@ public interface APIProvider extends APIManager {
      Map<String, String> propergateAPIStatusChangeToGateways(APIIdentifier identifier, String newStatus)
              throws APIManagementException;
 
-    Map<String, String> propergateAPIStatusChangeToGateways(API api, APIIdentifier identifier, String newStatus)
-            throws APIManagementException;
-
     /**
      * Push api related state changes to the gateway. Api related configurations will be deployed or destroyed
      * according to the new state.
@@ -868,11 +865,6 @@ public interface APIProvider extends APIManager {
       */
      boolean updateAPIforStateChange(APIIdentifier identifier, String newStatus,
              Map<String, String> failedGatewaysMap) throws APIManagementException, FaultGatewaysException;
-
-    boolean updateAPIforStateChange(API api, APIIdentifier identifier, String newStatus,
-                                    Map<String, String> failedGatewaysMap, GenericArtifactManager artifactManager,
-                                    GenericArtifact artifact) throws APIManagementException,
-            FaultGatewaysException;
 
     /**
      * Update api related information such as database entries, registry updates for state change.
