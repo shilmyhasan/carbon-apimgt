5f7202a9a7708bd8346bf6b6a3bcf0c510993c78
dushansilva <dushansilva95@gmail.com>
2021-03-01 15:13:43 +0530

    fix api pagination issue in alerts settings

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Settings/Alerts/Alerts.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Settings/Alerts/Alerts.jsx
@@ -74,6 +74,9 @@ const styles = theme => ({
     listItem: {
         marginLeft: theme.spacing(1),
     },
+    dialog: {
+        width: '100%',
+    },
 });
 
 /**
@@ -428,7 +431,7 @@ const Alerts = (props) => {
                             </React.Fragment>}
                     </React.Fragment>}
             </div>
-            <Dialog open={openDialog.open}>
+            <Dialog fullWidth maxWidth="md" open={openDialog.open}>
                 <DialogTitle>
                     <FormattedMessage
                         id='Settings.Alerts.Alerts.configure.alert'
