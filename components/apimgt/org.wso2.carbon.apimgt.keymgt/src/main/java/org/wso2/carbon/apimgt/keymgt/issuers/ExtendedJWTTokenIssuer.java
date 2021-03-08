b948fdc4a56e8b89fb5ca8cf9ba5578a94dc8e77
yasasrangika <rangikaymendis@gmail.com>
2021-03-09 00:40:53 +0530

    Allow users to change the access token life time

---
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/issuers/ExtendedJWTTokenIssuer.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.apimgt.keymgt.issuers;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.oauth.config.OAuthServerConfiguration;
+import org.wso2.carbon.identity.oauth.dao.OAuthAppDO;
+import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
+import org.wso2.carbon.identity.oauth2.token.JWTTokenIssuer;
+import org.wso2.carbon.identity.oauth2.token.OAuthTokenReqMessageContext;
+import org.wso2.carbon.identity.oauth2.token.handlers.grant.AuthorizationGrantHandler;
+
+public class ExtendedJWTTokenIssuer extends JWTTokenIssuer {
+
+    private static final Log log = LogFactory.getLog(JWTTokenIssuer.class);
+
+    public ExtendedJWTTokenIssuer() throws IdentityOAuth2Exception {
+    }
+
+    /**
+     * Handler to get token validity period for the Self contained JWT Access Token when it has custom expiration time.
+     *
+     * @param tokenReqMessageContext
+     * @param oAuthAppDO
+     * @param consumerKey
+     * @return lifetime in milli seconds
+     * @throws IdentityOAuth2Exception
+     */
+    public long getAccessTokenLifeTimeInMillis(OAuthTokenReqMessageContext tokenReqMessageContext,
+            OAuthAppDO oAuthAppDO, String consumerKey) throws IdentityOAuth2Exception {
+        long lifetimeInMillis;
+        boolean isUserAccessTokenType = isUserAccessTokenType(
+                tokenReqMessageContext.getOauth2AccessTokenReqDTO().getGrantType());
+
+        if (isUserAccessTokenType) {
+            lifetimeInMillis = oAuthAppDO.getUserAccessTokenExpiryTime() * 1000;
+            if (log.isDebugEnabled()) {
+                log.debug("User Access Token Life time set to : " + lifetimeInMillis + "ms.");
+            }
+        } else {
+            if (tokenReqMessageContext.getValidityPeriod() == -1) {
+                // the token request does not specify the validity period explicitly
+                lifetimeInMillis = oAuthAppDO.getApplicationAccessTokenExpiryTime() * 1000;
+            } else {
+                // set the expiry time sent in the request
+                lifetimeInMillis = tokenReqMessageContext.getValidityPeriod() * 1000;
+            }
+            if (log.isDebugEnabled()) {
+                log.debug("Application Access Token Life time set to : " + lifetimeInMillis + "ms.");
+            }
+        }
+
+        if (lifetimeInMillis == 0) {
+            if (isUserAccessTokenType) {
+                lifetimeInMillis =
+                        OAuthServerConfiguration.getInstance().getUserAccessTokenValidityPeriodInSeconds() * 1000;
+                if (log.isDebugEnabled()) {
+                    log.debug("User access token time was 0ms. Setting default user access token lifetime : "
+                            + lifetimeInMillis + "ms.");
+                }
+            } else {
+                lifetimeInMillis =
+                        OAuthServerConfiguration.getInstance().getApplicationAccessTokenValidityPeriodInSeconds()
+                                * 1000;
+                if (log.isDebugEnabled()) {
+                    log.debug("Application access token time was 0ms. Setting default Application access token "
+                            + "lifetime : " + lifetimeInMillis + "ms.");
+                }
+            }
+        }
+
+        if (log.isDebugEnabled()) {
+            log.debug("JWT Self Signed Access Token Life time set to : " + lifetimeInMillis + "ms.");
+        }
+        return lifetimeInMillis;
+    }
+
+    private boolean isUserAccessTokenType(String grantType) throws IdentityOAuth2Exception {
+        AuthorizationGrantHandler grantHandler = OAuthServerConfiguration.getInstance().getSupportedGrantTypes()
+                .get(grantType);
+        // If grant handler is null ideally we would not come to this point as the flow will be broken before. So we
+        // can guarantee grantHandler will not be null
+        return grantHandler.isOfTypeApplicationUser();
+    }
+}
