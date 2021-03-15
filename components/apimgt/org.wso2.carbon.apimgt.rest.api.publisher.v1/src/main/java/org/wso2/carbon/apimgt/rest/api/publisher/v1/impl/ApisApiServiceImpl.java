bcd98170b556c70c4bf27f7afe737788715d9ad6
shilmyhasan <silmy@wso2.com>
2021-03-15 11:37:07 +0530

    Generate sequence on swagger update

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/impl/ApisApiServiceImpl.java
@@ -2880,6 +2880,7 @@ public class ApisApiServiceImpl implements ApisApiService {
         APIDefinition oasParser = response.getParser();
         String apiDefinition = response.getJsonContent();
         apiDefinition = OASParserUtil.preProcess(apiDefinition);
+        SequenceGenerator.generateSequencesFromSwagger(apiDefinition, existingAPI.getId());
         Set<URITemplate> uriTemplates = null;
         try {
             uriTemplates = oasParser.getURITemplates(apiDefinition);
