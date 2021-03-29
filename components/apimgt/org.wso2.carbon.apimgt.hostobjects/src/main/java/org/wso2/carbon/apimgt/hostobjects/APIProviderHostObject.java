2fa36dfe29448dfcc855b2b3050d34207de5ca75
Hasunie <hasunie@wso2.com>
2021-03-29 09:53:27 +0530

    added document uploading restriction

--- components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/APIProviderHostObject.java
+++ components/apimgt/org.wso2.carbon.apimgt.hostobjects/src/main/java/org/wso2/carbon/apimgt/hostobjects/APIProviderHostObject.java
@@ -148,6 +148,8 @@ public class APIProviderHostObject extends ScriptableObject {
     private static String ICON_PATH = "tmp/icon";
     private static final String ALIAS = "alias";
     private static final String END_POINT = "endpoint";
+    private static final String EXT_HTML = "html";
+    private static final String EXT_HTM = "htm";
 
     private APIProvider apiProvider;
 
@@ -3427,6 +3429,9 @@ public class APIProviderHostObject extends ScriptableObject {
                 if (extension.contains("exe")) {
                     throw new APIManagementException("File type .exe is not supported!");
                 }
+                if ((extension.contains(EXT_HTML) || (extension.contains(EXT_HTM)))) {
+                    throw new APIManagementException("File type .html is not supported!");
+                }
                 String contentType = (String) args[10];
                 apiProvider
                         .addFileToDocumentation(apiId, doc, fileHostObject.getName(), fileHostObject.getInputStream(),
@@ -4091,6 +4096,9 @@ public class APIProviderHostObject extends ScriptableObject {
                     String extension = FilenameUtils.getExtension(fileHostObject.getJavaScriptFile().getName());
                     if (extension.contains("exe")) {
                         throw new APIManagementException("File type .exe is not supported!");
+                    }
+                    if ((extension.contains(EXT_HTML) || (extension.contains(EXT_HTM)))) {
+                        throw new APIManagementException("File type .html is not supported!");
                     }
                     ResourceFile resourceFile = new ResourceFile(fileHostObject.getInputStream(),
                             fileHostObject.getJavaScriptFile().getContentType());
