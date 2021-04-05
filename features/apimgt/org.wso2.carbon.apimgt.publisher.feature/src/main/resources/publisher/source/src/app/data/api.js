087f05697835b89f667e47ae6563888670b25830
Kavishka <kavishkafdo95@gmail.com>
2021-04-05 12:44:37 +0530

    Fixes mediation policy limit issue

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/data/api.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/data/api.js
@@ -19,6 +19,7 @@ import APIClientFactory from './APIClientFactory';
 import Utils from './Utils';
 import Resource from './Resource';
 import cloneDeep from 'lodash.clonedeep';
+import Configurations from 'Config';
 
 /**
  * An abstract representation of an API
@@ -1952,10 +1953,12 @@ class API extends Resource {
      */
     static getMediationPolicies(apiId) {
         const restApiClient = new APIClientFactory().getAPIClient(Utils.getCurrentEnvironment()).client;
+        const limit = Configurations.app.mediationPolicyCount;
         return restApiClient.then(client => {
             return client.apis['API Mediation Policies'].apisApiIdMediationPoliciesGet(
                 {
                     apiId: apiId,
+                    limit: limit,
                 },
                 this._requestMetaData(),
             );
@@ -2076,8 +2079,9 @@ class API extends Resource {
      */
     static getGlobalMediationPolicies() {
         const restApiClient = new APIClientFactory().getAPIClient(Utils.getCurrentEnvironment()).client;
+        const limit = Configurations.app.mediationPolicyCount;
         return restApiClient.then(client => {
-            return client.apis['Global Mediation Policies'].getAllGlobalMediationPolicies({}, this._requestMetaData());
+            return client.apis['Global Mediation Policies'].getAllGlobalMediationPolicies( {limit}, this._requestMetaData());
         });
     }
 
