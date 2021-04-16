2030fae5f61eb7689f8085603ae410f3b5e499b5
Chanaka Jayasena <chanaka3d@gmail.com>
2021-04-16 10:25:48 +0530

    Fixing https://github.com/wso2/product-apim/issues/10140 for applicationId

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/SubscriptionsApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/SubscriptionsApiServiceImpl.java
@@ -122,7 +122,7 @@ public class SubscriptionsApiServiceImpl implements SubscriptionsApiService {
                         .fromSubscriptionListToDTO(subscribedAPIList, limit, offset);
 
                 //to retrieve pagination parameter
-                SubscriptionMappingUtil.setPaginationParams(subscriptionListDTO, apiId, "", limit,
+                SubscriptionMappingUtil.setPaginationParams(subscriptionListDTO, apiId, null,"", limit,
                         offset, subscribedAPIList.size());
                 
                 return Response.ok().entity(subscriptionListDTO).build();
@@ -144,6 +144,11 @@ public class SubscriptionsApiServiceImpl implements SubscriptionsApiService {
                 
                 subscriptionListDTO = SubscriptionMappingUtil.fromSubscriptionListToDTO(subscribedAPIList, limit,
                         offset);
+
+                //to retrieve pagination parameter
+                SubscriptionMappingUtil.setPaginationParams(subscriptionListDTO,null, applicationId, groupId,
+                        limit, offset, subscribedAPIList.size());
+
                 return Response.ok().entity(subscriptionListDTO).build();
 
             } else {
