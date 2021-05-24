736dd748c648788b511ae5b7aff02dcedaa5e299
Samitha Chathuranga <rmschathuranga@gmail.com>
2021-05-24 18:21:08 +0530

    Adding extension validation for API document adding for v0.15 REST api

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ApisApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/impl/ApisApiServiceImpl.java
@@ -1541,6 +1541,13 @@ public class ApisApiServiceImpl extends ApisApiService {
                 if (!documentation.getSourceType().equals(Documentation.DocumentSourceType.FILE)) {
                     RestApiUtil.handleBadRequest("Source type of document " + documentId + " is not FILE", log);
                 }
+                String filename = fileDetail.getContentDisposition().getFilename();
+                if (filename.endsWith(".pdf") || filename.endsWith(".txt") || filename.endsWith(".doc") || filename
+                                                .endsWith(".docx")) {
+                    RestApiPublisherUtils.attachFileToDocument(apiId, documentation, inputStream, fileDetail);
+                } else {
+                    RestApiUtil.handleBadRequest("Unsupported extension type of document file: " + filename, log);
+                }
                 RestApiPublisherUtils.attachFileToDocument(apiId, documentation, inputStream, fileDetail);
             } else if (inlineContent != null) {
                 if (!documentation.getSourceType().equals(Documentation.DocumentSourceType.INLINE)) {
