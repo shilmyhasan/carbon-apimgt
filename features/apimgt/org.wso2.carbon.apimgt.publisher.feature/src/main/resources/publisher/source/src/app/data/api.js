47995c0d608eed21b6b439cae2d9c02fa041910b
Chamindu36 <cudakara@gmail.com>
2021-06-03 16:31:11 +0530

    Address review comments

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/data/api.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/data/api.js
@@ -1788,10 +1788,11 @@ class API extends Resource {
      * Get the available policies information by tier level.
      * @param {String} policyLevel List API or Application or Resource type policies.parameter should be one
      * of api, application, subscription and resource
+     * @param limit
      * @returns {Promise}
      *
      */
-    static policies(policyLevel, limit = Configurations.app.throttlingPolicyLimit) {
+    static policies(policyLevel, limit ) {
         const apiClient = new APIClientFactory().getAPIClient(Utils.getCurrentEnvironment()).client;
         return apiClient.then(client => {
             return client.apis['Throttling Policies'].getAllThrottlingPolicies(
