b05028d21782974fcf7e284d7f6dbfd057f869c7
Chanaka Jayasena <chanaka3d@gmail.com>
2021-06-14 16:04:52 +0530

    Fixing https://github.com/wso2/product-apim/issues/11315

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Settings/Alerts/Alerts.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Settings/Alerts/Alerts.jsx
@@ -202,7 +202,9 @@ const Alerts = (props) => {
      * @param {string} email The email address that is being added.
      * */
     const handleAddEmail = (email) => {
-        setEmailsList(email);
+        const newEmail = [...emails];
+        newEmail.push(email);
+        setEmailsList(newEmail);
     };
 
     /**
@@ -211,10 +213,7 @@ const Alerts = (props) => {
      * @param {string} email : The email that is being deleted.
      * */
     const handleEmailDeletion = (email) => {
-        const newEmails = emails.filter((oldEmail) => {
-            return oldEmail !== email;
-        });
-        setEmailsList(newEmails);
+        setEmailsList([...emails].filter((oldEmail) => oldEmail !== email));
     };
 
     useEffect(() => {
@@ -369,7 +368,7 @@ const Alerts = (props) => {
                                             helperText={'Email address to receive alerts of selected'
                                                 + ' Alert types. Type email'
                                                 + ' address and press Enter to add'}
-                                            onChange={(chip) => {
+                                            onAdd={(chip) => {
                                                 handleAddEmail(chip);
                                             }}
                                             onDelete={(chip) => {
@@ -417,7 +416,7 @@ const Alerts = (props) => {
                         </>
                     )}
             </Paper>
-            <Dialog fullWidth maxWidth='md' open={openDialog.open}>
+            <Dialog open={openDialog.open}>
                 <DialogTitle>
                     <FormattedMessage
                         id='Settings.Alerts.Alerts.configure.alert'
