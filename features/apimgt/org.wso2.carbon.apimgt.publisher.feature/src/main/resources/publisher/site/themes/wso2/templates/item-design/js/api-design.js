fa59e1d42041cc7929fed76317ee6c82d6bc9003
HeshanSudarshana <khsudarshana@gmail.com>
2021-08-28 00:50:55 +0530

    Sync U2 resources files with WUM

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/item-design/js/api-design.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/themes/wso2/templates/item-design/js/api-design.js
@@ -148,6 +148,10 @@ function APIDesigner(){
         if(path.charAt(0) != "/")
             path = "/"+path;
 
+        if (path.endsWith("/")) {
+            path = path.slice(0, -1)
+        }
+
     	var resource_exist = false;
         $(".http_verb_select").each(function(){    //added this validation to fix https://wso2.org/jira/browse/APIMANAGER-2671
             if($(this).is(':checked')){
@@ -263,6 +267,17 @@ APIDesigner.prototype.check_if_resource_exist = function(path, method){
     return false;
 }
 
+APIDesigner.prototype.remove_trailing_slash = function(swagger) {
+    var paths = swagger.paths;
+    for (var path in paths) {
+        if (path.endsWith("/")) {
+            var newkey = path.slice(0, -1);
+            swagger.paths[newkey] = swagger.paths[path];
+            delete swagger.paths[path];
+        }
+    }
+    return swagger;
+};
 
 APIDesigner.prototype.set_default_management_values = function(){
     var operations = this.query("$.paths.*.*");
@@ -562,7 +577,8 @@ APIDesigner.prototype.load_api_document = function(api_document){
 
 APIDesigner.prototype.load_swagger_editor_content = function (){
     if(this.api_doc != ""){
-        var swagYaml = jsyaml.safeDump(this.api_doc);
+        var swagger = jQuery.extend(true, {}, this.api_doc);
+        var swagYaml = jsyaml.safeDump(this.remove_trailing_slash(swagger));
         window.localStorage.setItem(SWAGGER_CONTENT, swagYaml);
         window.localStorage.setItem(SWAGGER_CONTENT_CACHE, swagYaml);
     }
@@ -571,7 +587,7 @@ APIDesigner.prototype.load_swagger_editor_content = function (){
 APIDesigner.prototype.render_scopes = function(){
     if($('#scopes-template').length){
         context = {
-            "api_doc" : this.api_doc
+            "doc" : this.api_doc
         }
         var output = Handlebars.partials['scopes-template'](context);
         $('#scopes_view').html(output);
@@ -579,7 +595,7 @@ APIDesigner.prototype.render_scopes = function(){
 };
 
 APIDesigner.prototype.transform = function(api_doc){
-    var swagger = jQuery.extend(true, {}, this.api_doc);
+    var swagger = jQuery.extend(true, {}, api_doc);
     for(var pathkey in swagger.paths){
         var path = swagger.paths[pathkey];
         var parameters = path.parameters;
@@ -595,7 +611,7 @@ APIDesigner.prototype.transform = function(api_doc){
             verb.path = pathkey;
         }
     }
-    return swagger;
+    return this.remove_trailing_slash(swagger)
 }
 
 APIDesigner.prototype.setApiLevelPolicy = function(isAPILevel){
@@ -603,8 +619,9 @@ APIDesigner.prototype.setApiLevelPolicy = function(isAPILevel){
 }
 
 APIDesigner.prototype.render_resources = function(){
+    var json = jsyaml.safeLoad(window.localStorage.getItem(SWAGGER_CONTENT));
     context = {
-        "doc" : this.transform(this.api_doc),
+        "doc" : this.transform(json),
         "verbs" :VERBS,
         "has_resources" : this.has_resources()
     }
