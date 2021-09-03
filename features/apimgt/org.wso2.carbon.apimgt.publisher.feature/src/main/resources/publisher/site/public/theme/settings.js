0255cd15680eb3adcd4bc522828e3c525588166b
Tharika Madurapperuma <tharika.m92@gmail.com>
2021-09-03 17:53:59 +0530

    fixed JSON and ZIP file support for API Documentation

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/public/theme/settings.js
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/site/public/theme/settings.js
@@ -11,5 +11,9 @@ const Settings = {
         markdown: {
             skipHtml: true,
         },
+        supportedDocTypes: 'application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document,'
+        + ' application/pdf, text/plain, application/vnd.ms-excel, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,'
+        + ' application/vnd.oasis.opendocument.text, application/vnd.oasis.opendocument.spreadsheet,'
+        + ' application/json, application/x-yaml, .md',
     },
 };
