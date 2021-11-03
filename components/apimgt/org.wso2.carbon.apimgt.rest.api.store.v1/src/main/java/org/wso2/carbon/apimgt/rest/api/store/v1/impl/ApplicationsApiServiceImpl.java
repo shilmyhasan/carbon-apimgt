8b5d94078d8a86d5a7469d99026305e5f7e72239
hisanhunais <hisan.live@gmail.com>
2021-11-03 10:30:02 +0530

    Handle token type changes when updating applications

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/ApplicationsApiServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.store.v1/src/main/java/org/wso2/carbon/apimgt/rest/api/store/v1/impl/ApplicationsApiServiceImpl.java
@@ -293,12 +293,6 @@ public class ApplicationsApiServiceImpl implements ApplicationsApiService {
             if (applicationAttributes != null) {
                 body.setAttributes(applicationAttributes);
             }
-
-            //we do not honor tokenType sent in the body and all the applications are considered of 'JWT' token type
-            //unless the current application is already of 'OAUTH' type
-            if (!ApplicationDTO.TokenTypeEnum.OAUTH.toString().equals(oldApplication.getTokenType())) {
-                body.setTokenType(ApplicationDTO.TokenTypeEnum.JWT);
-            }
             
             //we do not honor the subscriber coming from the request body as we can't change the subscriber of the application
             Application application = ApplicationMappingUtil.fromDTOtoApplication(body, username);
