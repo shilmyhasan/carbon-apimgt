9300ef446864c3c0450894acabd30b13ff523422
Prasanna Dangalla <prasannadangalla@gmail.com>
2022-02-22 13:08:15 +0530

    Haddled a null pointer exception for https://github.com/wso2/product-apim/issues/12429

--- components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/DefaultKeyValidationHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.keymgt/src/main/java/org/wso2/carbon/apimgt/keymgt/handlers/DefaultKeyValidationHandler.java
@@ -261,6 +261,9 @@ public class DefaultKeyValidationHandler extends AbstractKeyValidationHandler {
 
             if (keyManagerInstance != null) {
                 AccessTokenInfo tokenInfo = keyManagerInstance.getTokenMetaData(validationContext.getAccessToken());
+                if (tokenInfo == null) {
+                    return null;
+                }
                 tokenInfo.setKeyManager(electedKeyManager);
                 CacheProvider.getGatewayIntrospectCache().put(validationContext.getAccessToken(), tokenInfo);
                 return tokenInfo;
