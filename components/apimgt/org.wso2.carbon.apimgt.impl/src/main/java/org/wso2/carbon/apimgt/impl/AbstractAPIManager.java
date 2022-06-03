7062478320351b3a49d45323d7454d612f6b22fa
Crowley Rajapakse <srrajapakse1@gmail.com>
2022-06-03 17:36:10 +0400

    fixing API categories getting executed while migrating issue

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/AbstractAPIManager.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/AbstractAPIManager.java
@@ -191,6 +191,7 @@ public abstract class AbstractAPIManager implements APIManager {
     private LRUCache<String, GenericArtifactManager> genericArtifactCache = new LRUCache<String, GenericArtifactManager>(
             5);
     private org.wso2.carbon.apimgt.persistence.dto.ResourceFile wsdl;
+    String migrationEnabled = System.getProperty(APIConstants.MIGRATE);
 
     public AbstractAPIManager() throws APIManagementException {
 
@@ -3683,8 +3684,10 @@ public abstract class AbstractAPIManager implements APIManager {
                 // category array retrieved from artifact has only the category name, therefore we need to fetch
                 // categories
                 // and fill out missing attributes before attaching the list to the api
-                List<APICategory> allCategories = APIUtil.getAllAPICategoriesOfOrganization(organization);
-
+                List<APICategory> allCategories = new ArrayList<>();
+                if (migrationEnabled == null) {
+                    allCategories = APIUtil.getAllAPICategoriesOfOrganization(organization);
+                }
                 // todo-category: optimize this loop with breaks
                 for (String categoryName : categoriesOfAPI) {
                     for (APICategory category : allCategories) {
