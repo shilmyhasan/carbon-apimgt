5604ff64ec8d1667ca31a1ec835a991b5e9ef37b
Tharanidk <tharani.dayadhi@gmail.com>
2022-11-18 13:13:54 +0530

    NPE handled

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/throttling/ThrottleConditionEvaluator.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/throttling/ThrottleConditionEvaluator.java
@@ -223,7 +223,8 @@ public class ThrottleConditionEvaluator {
 
     private boolean isJWTClaimPresent(AuthenticationContext authenticationContext, ConditionDto.JWTClaimConditions
             condition) {
-
+        if (authenticationContext.getCallerToken()== null)
+            return false;
         Map<String, String> assertions = JWTUtil.getJWTClaims(authenticationContext.getCallerToken());
         boolean status = true;
 
