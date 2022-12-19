f5d67ee39a3b097f283cf9ff312c142277a22af6
binodmx <binod@wso2.com>
2022-12-19 15:05:46 +0530

    Fix not setting token type when workflows enabled

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/constants/SQLConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/constants/SQLConstants.java
@@ -1889,6 +1889,7 @@ public class SQLConstants {
             "   APP.NAME," +
             "   APP.SUBSCRIBER_ID," +
             "   APP.APPLICATION_TIER," +
+            "   APP.TOKEN_TYPE AS APP_TOKEN_TYPE," +
             "   REG.TOKEN_TYPE," +
             "   REG.TOKEN_SCOPE," +
             "   APP.CALLBACK_URL," +
