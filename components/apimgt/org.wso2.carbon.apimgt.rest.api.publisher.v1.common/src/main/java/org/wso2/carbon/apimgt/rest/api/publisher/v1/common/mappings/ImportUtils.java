f2761ed320f416ff49ff55204526222c0f177ac4
1akshitha <lakshitha@wso2.com>
2023-01-10 10:39:49 +0530

    Fix API import when publisher access control enabled
    When importing multiple versions of APIs with publisher
    access control enabled by a different user than the API
    provider, role validation is done based on the provider.
    Previously provider was taken from the artefact which
    was the original user and if that user does not exist in the
    new environment, there were multiple errors and this
    sets the provider to the logged in user.
    
    Resolves: https://github.com/wso2/api-manager/issues/1020

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/mappings/ImportUtils.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1.common/src/main/java/org/wso2/carbon/apimgt/rest/api/publisher/v1/common/mappings/ImportUtils.java
@@ -1932,7 +1932,7 @@ public class ImportUtils {
                 }
             }
 
-            APIProvider apiProvider = RestApiCommonUtil.getProvider(importedApiProductDTO.getProvider());
+            APIProvider apiProvider = RestApiCommonUtil.getLoggedInUserProvider();
 
             // Check whether the API resources are valid
             checkAPIProductResourcesValid(extractedFolderPath, userName, apiProvider, importedApiProductDTO,
