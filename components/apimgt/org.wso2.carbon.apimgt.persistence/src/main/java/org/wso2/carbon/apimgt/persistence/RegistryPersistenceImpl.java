a1640e3782e79b5990008692e73080afc523abf0
rusirijayodaillesinghe <rusiri@wso2.com>
2023-01-16 18:13:39 +0530

    Move revision directory deletion logic into a separate function Solves review comment in https://github.com/wso2-support/carbon-apimgt/pull/5451 Public issue : https://github.com/wso2/api-manager/issues/862

--- components/apimgt/org.wso2.carbon.apimgt.persistence/src/main/java/org/wso2/carbon/apimgt/persistence/RegistryPersistenceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.persistence/src/main/java/org/wso2/carbon/apimgt/persistence/RegistryPersistenceImpl.java
@@ -879,13 +879,7 @@ public class RegistryPersistenceImpl implements APIPersistence {
                     registry.delete(apiProviderPath);
                 }
             }
-
-            /*remove revision directory with UUID*/
-            String revisionDirectoryPath = APIConstants.API_REVISION_LOCATION + RegistryConstants.PATH_SEPARATOR +
-                    apiId;
-            if (registry.resourceExists(revisionDirectoryPath)) {
-                registry.delete(revisionDirectoryPath);
-            }
+            deleteApiRevisionDirectory(apiId, registry);
 
             registry.commitTransaction();
             transactionCommitted  = true;
@@ -902,6 +896,19 @@ public class RegistryPersistenceImpl implements APIPersistence {
             } catch (RegistryException ex) {
                 throw new APIPersistenceException("Error occurred while rolling back the transaction. ", ex);
             }
+        }
+    }
+
+    /**
+     * Remove revision directory with UUID.
+     * @param apiId String
+     * @param registry Registry
+     **/
+    private void deleteApiRevisionDirectory(String apiId, Registry registry) throws RegistryException {
+        String revisionDirectoryPath = APIConstants.API_REVISION_LOCATION + RegistryConstants.PATH_SEPARATOR +
+                apiId;
+        if (registry.resourceExists(revisionDirectoryPath)) {
+            registry.delete(revisionDirectoryPath);
         }
     }
 
@@ -3540,12 +3547,7 @@ public class RegistryPersistenceImpl implements APIPersistence {
                 }
             }
 
-            /*remove revision directory with UUID*/
-            String revisionDirectoryPath = APIConstants.API_REVISION_LOCATION + RegistryConstants.PATH_SEPARATOR +
-                    apiId;
-            if (registry.resourceExists(revisionDirectoryPath)) {
-                registry.delete(revisionDirectoryPath);
-            }
+           deleteApiRevisionDirectory(apiId, registry);
 
         } catch (RegistryException e) {
             String msg = "Failed to get API";
