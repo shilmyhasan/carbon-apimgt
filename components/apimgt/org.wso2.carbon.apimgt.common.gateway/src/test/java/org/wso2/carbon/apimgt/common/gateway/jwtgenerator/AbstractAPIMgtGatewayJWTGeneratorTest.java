db4db0b80659fedb1a08667ecc19baed8d06738d
Suneth Samarasinghe <imsuneth@gmail.com>
2023-03-16 09:57:13 +0530

    Add unit tests for base64 encode

---
+++ components/apimgt/org.wso2.carbon.apimgt.common.gateway/src/test/java/org/wso2/carbon/apimgt/common/gateway/jwtgenerator/AbstractAPIMgtGatewayJWTGeneratorTest.java
@@ -0,0 +1,22 @@
+package org.wso2.carbon.apimgt.common.gateway.jwtgenerator;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.wso2.carbon.apimgt.common.gateway.exception.JWTGeneratorException;
+
+public class AbstractAPIMgtGatewayJWTGeneratorTest {
+
+    @Test
+    public void testEncode() {
+        // Test whether the encode method is base64 encoding.
+        APIMgtGatewayJWTGeneratorImpl apiMgtGatewayJWTGenerator = new APIMgtGatewayJWTGeneratorImpl();
+        String stringToBeEncoded = "<<???>>";
+        String expectedEncodedString = "PDw/Pz8+PiA=";
+        try {
+            String actualEncodedString = apiMgtGatewayJWTGenerator.encode(stringToBeEncoded.getBytes());
+            Assert.assertEquals(expectedEncodedString, actualEncodedString);
+        } catch (JWTGeneratorException e) {
+            Assert.fail("JWTGeneratorException thrown");
+        }
+    }
+}
