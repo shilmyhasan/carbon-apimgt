54437daf1a576a3c3d00d0b84b98f5db813ac1a2
Prasanna Dangalla <prasannadangalla@gmail.com>
2023-05-16 07:55:27 +0530

    Added import.

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/analytics/APIMgtGoogleAnalyticsTrackingHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/analytics/APIMgtGoogleAnalyticsTrackingHandler.java
@@ -53,6 +53,7 @@ import org.wso2.carbon.apimgt.tracing.Util;
 import org.wso2.carbon.ganalytics.publisher.GoogleAnalyticsConstants;
 import org.wso2.carbon.ganalytics.publisher.GoogleAnalyticsData;
 import org.wso2.carbon.ganalytics.publisher.GoogleAnalyticsDataPublisher;
+import org.wso2.carbon.ganalytics.publisher.ga4.event.PageViewEvent;
 
 public class APIMgtGoogleAnalyticsTrackingHandler extends AbstractHandler {
 
