21e00c0a0475ad930582fb76827ce4bb5dc56f91
msm1992 <madusha.sachini@yahoo.com>
2023-05-31 09:30:00 +0530

    Fix issue

--- features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/Resources/components/operationComponents/AWSLambdaSettings.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.publisher.feature/src/main/resources/publisher/source/src/app/components/Apis/Details/Resources/components/operationComponents/AWSLambdaSettings.jsx
@@ -171,7 +171,8 @@ export default function IntegrationDownshift(props) {
     useEffect(() => {
         if (operation['x-amzn-resource-timeout']) {
             setTimeout(operation['x-amzn-resource-timeout']);
-        } else if (operation['x-amzn-resource-content-encode']) {
+        }
+        if (operation['x-amzn-resource-content-encode']) {
             setIsBase64Encoded(operation['x-amzn-resource-content-encode']);
         }
     }, []);
