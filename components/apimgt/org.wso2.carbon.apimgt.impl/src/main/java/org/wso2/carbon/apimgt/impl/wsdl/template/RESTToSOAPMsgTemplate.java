7eb76897e7cddf869033af8c7114088f262a4aac
dhanushka madushan <madushandhanushka@gmail.com>
2023-06-16 10:38:02 +0530

    Fix velocity template load issue in windows wrapper

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/wsdl/template/RESTToSOAPMsgTemplate.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/wsdl/template/RESTToSOAPMsgTemplate.java
@@ -68,7 +68,7 @@ public class RESTToSOAPMsgTemplate {
             context.internalGetKeys();
 
             VelocityEngine velocityengine = new VelocityEngine();
-            APIUtil.initializeVelocityContext(velocityengine);
+            APIUtil.initializeVelocityContextClassPath(velocityengine);
             velocityengine.init();
             org.apache.velocity.Template t = velocityengine.getTemplate(this.getInSeqTemplatePath());
             t.merge(context, writer);
@@ -92,7 +92,7 @@ public class RESTToSOAPMsgTemplate {
             context.internalGetKeys();
 
             VelocityEngine velocityengine = new VelocityEngine();
-            APIUtil.initializeVelocityContext(velocityengine);
+            APIUtil.initializeVelocityContextClassPath(velocityengine);
             velocityengine.init();
             Template template = velocityengine.getTemplate(this.getOutSeqTemplatePath());
 
