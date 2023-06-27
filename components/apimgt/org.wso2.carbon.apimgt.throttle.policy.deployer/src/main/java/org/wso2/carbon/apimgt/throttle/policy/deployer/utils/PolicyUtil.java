ab2725896bff061cfbb6e9458a6054ca30215d41
GihanAyesh <gihanayeshmanthadrck@gmail.com>
2023-06-27 16:35:39 +0530

    get all policies from CP before undeploying

--- components/apimgt/org.wso2.carbon.apimgt.throttle.policy.deployer/src/main/java/org/wso2/carbon/apimgt/throttle/policy/deployer/utils/PolicyUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.throttle.policy.deployer/src/main/java/org/wso2/carbon/apimgt/throttle/policy/deployer/utils/PolicyUtil.java
@@ -151,12 +151,15 @@ public class PolicyUtil {
      * Deploy all the throttle policies retrieved from the database in the Traffic Manager.
      */
     public static void deployAllPolicies() {
-        // Undeploy all existing policies
-        undeployAllPolicies();
         PolicyRetriever policyRetriever = new PolicyRetriever();
         try {
             // Deploy all the policies retrieved from the database
             SubscriptionPolicyList subscriptionPolicies = policyRetriever.getAllSubscriptionPolicies();
+            ApplicationPolicyList applicationPolicies = policyRetriever.getAllApplicationPolicies();
+            ApiPolicyList apiPolicies = policyRetriever.getAllApiPolicies();
+            GlobalPolicyList globalPolicies = policyRetriever.getAllGlobalPolicies();
+            // Undeploy all existing policies
+            undeployAllPolicies();
             for (SubscriptionPolicy subscriptionPolicy : subscriptionPolicies.getList()) {
                 if (!(APIConstants.UNLIMITED_TIER.equalsIgnoreCase(subscriptionPolicy.getName())
                         || APIConstants.DEFAULT_SUB_POLICY_ASYNC_UNLIMITED.
@@ -166,19 +169,16 @@ public class PolicyUtil {
                     deployPolicy(subscriptionPolicy, null);
                 }
             }
-            ApplicationPolicyList applicationPolicies = policyRetriever.getAllApplicationPolicies();
             for (ApplicationPolicy applicationPolicy : applicationPolicies.getList()) {
                 if (!APIConstants.UNLIMITED_TIER.equalsIgnoreCase(applicationPolicy.getName())) {
                     deployPolicy(applicationPolicy, null);
                 }
             }
-            ApiPolicyList apiPolicies = policyRetriever.getAllApiPolicies();
             for (ApiPolicy apiPolicy : apiPolicies.getList()) {
                 if (!APIConstants.UNLIMITED_TIER.equalsIgnoreCase(apiPolicy.getName())) {
                     deployPolicy(apiPolicy, null);
                 }
             }
-            GlobalPolicyList globalPolicies = policyRetriever.getAllGlobalPolicies();
             for (GlobalPolicy globalPolicy : globalPolicies.getList()) {
                 deployPolicy(globalPolicy, null);
             }
