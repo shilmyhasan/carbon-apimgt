eb00863e4d13be47e9be3976f64456b750905c1b
msm1992 <madusha.sachini@yahoo.com>
2023-08-02 11:00:23 +0530

    Fix null in WWW-Authenticate header

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/APIAuthenticationHandler.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/handlers/security/APIAuthenticationHandler.java
@@ -573,7 +573,7 @@ public class APIAuthenticationHandler extends AbstractHandler implements Managed
         StringBuilder challengeString = new StringBuilder();
         if (authenticators != null) {
             for (Authenticator authenticator : authenticators) {
-                challengeString.append(authenticator.getChallengeString()).append(" ");
+                challengeString.append(authenticator.getChallengeString()).append(", ");
             }
         }
         return challengeString.toString().trim();
