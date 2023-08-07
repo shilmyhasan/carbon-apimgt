dd74a873665c6192478a1e925bd2884d0fca775e
hiranyakavishani <hiranyakavi@gmail.com>
2023-08-07 18:02:51 +0530

    Fixing request/response owner consistent in DCR calls for carbon.super

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.dcr/src/main/java/org/wso2/carbon/apimgt/rest/api/dcr/web/impl/RegistrationServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.dcr/src/main/java/org/wso2/carbon/apimgt/rest/api/dcr/web/impl/RegistrationServiceImpl.java
@@ -315,7 +315,6 @@ public class RegistrationServiceImpl implements RegistrationService {
                     getOAuthApplicationDataByAppName(applicationName);
             Map<String, String> valueMap = new HashMap<String, String>();
             valueMap.put(OAUTH_CLIENT_GRANT, consumerAppDTO.getGrantTypes());
-
             String appOwner = consumerAppDTO.getUsername();
 
             if (MultitenantConstants.SUPER_TENANT_DOMAIN_NAME.equals(
