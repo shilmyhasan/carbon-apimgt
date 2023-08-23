1c107e40b64ee1d3b638cda2f52996c3bf8859da
HeshanSudarshana <khsudarshana@gmail.com>
2023-08-23 12:06:03 +0530

    Add null check when setting timeoutInMillis

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OASParserUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OASParserUtil.java
@@ -1188,15 +1188,14 @@ public class OASParserUtil {
             endpointResult.set(APIConstants.ENDPOINT_URLS, endpointsArray);
             endpointResult.put(APIConstants.X_WSO2_ENDPOINT_TYPE, type);
             if (primaryEndpoints.has(APIConstants.ADVANCE_ENDPOINT_CONFIG)) {
-                JSONObject advanceEndpointConfig =
-                        primaryEndpoints.getJSONObject(APIConstants.ADVANCE_ENDPOINT_CONFIG);
+                JSONObject advanceEndpointConfig = primaryEndpoints.getJSONObject(APIConstants.ADVANCE_ENDPOINT_CONFIG);
                 ObjectNode advanceEndpointsObject = objectMapper.createObjectNode();
-                if (advanceEndpointConfig.has(APIConstants.TIMEOUT_IN_MILLIS)) {
+                if (advanceEndpointConfig.has(APIConstants.TIMEOUT_IN_MILLIS)
+                        && advanceEndpointConfig.get(APIConstants.TIMEOUT_IN_MILLIS) != JSONObject.NULL) {
                     advanceEndpointsObject.put(APIConstants.TIMEOUT_IN_MILLIS,
                             advanceEndpointConfig.getInt(APIConstants.TIMEOUT_IN_MILLIS));
+                    endpointResult.set(APIConstants.ADVANCE_ENDPOINT_CONFIG, advanceEndpointsObject);
                 }
-                endpointResult.set(APIConstants.ADVANCE_ENDPOINT_CONFIG,
-                        advanceEndpointsObject);
             }
             return endpointResult;
         }
