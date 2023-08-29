f87f5e5fab7ea9e33820623dfe10ec37256fe212
Prasanna Dangalla <prasannadangalla@gmail.com>
2023-08-29 15:45:46 +0530

    Fix for - 'Provide Existing OAuth Keys' button will become inactive for reset of the Key Managers once keys are generated under one of the Key Managers of the same application.'

--- features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Shared/AppsAndKeys/TokenManager.jsx
+++ features/apimgt/org.wso2.carbon.apimgt.store.feature/src/main/resources/devportal/source/src/app/components/Shared/AppsAndKeys/TokenManager.jsx
@@ -284,7 +284,7 @@ class TokenManager extends React.Component {
                 || availableGrantTypes.filter((type) => (type !== 'authorization_code' && type !== 'implicit')),
                 additionalProperties: additionalProperties || this.getDefaultAdditionalProperties(selectedKM),
             };
-            this.setState({ keyRequest: newRequest, selectedTab: newSelectedTab, mode: mode, importDisabled: (mode === 'MAPPED' || mode === 'CREATED')});
+            this.setState({ keyRequest: newRequest, selectedTab: newSelectedTab, mode: mode, importDisabled: (mode === 'MAPPED' || mode === 'CREATED') });
         } else {
             // Fill the keyRequest.additionalProperties from the selectedKM.applicationConfiguration defaultValues.
             this.setState({
