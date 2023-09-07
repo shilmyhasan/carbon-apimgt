b5821007c4bc41ff86a5fa6bc6f72fc10eb10072
Prasanna Dangalla <prasannadangalla@gmail.com>
2023-09-07 16:33:52 +0530

    Used appOwner variable.

--- components/apimgt/org.wso2.carbon.apimgt.rest.api.dcr/src/main/java/org/wso2/carbon/apimgt/rest/api/dcr/web/impl/RegistrationServiceImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.rest.api.dcr/src/main/java/org/wso2/carbon/apimgt/rest/api/dcr/web/impl/RegistrationServiceImpl.java
@@ -329,9 +329,8 @@ public class RegistrationServiceImpl implements RegistrationService {
 
             appToReturn = this.fromAppDTOToApplicationInfo(consumerAppDTO.getOauthConsumerKey(),
                     consumerAppDTO.getApplicationName(), consumerAppDTO.getCallbackUrl(),
-                    consumerAppDTO.getOauthConsumerSecret(), saasApp,
-                    MultitenantUtils.getTenantAwareUsername(consumerAppDTO.getUsername()), valueMap);
-
+                    consumerAppDTO.getOauthConsumerSecret(), saasApp, appOwner, consumerAppDTO.getTokenType(),
+                    valueMap);
         } catch (IdentityOAuthAdminException e) {
             log.error("error occurred while trying to get OAuth Application data", e);
         }
