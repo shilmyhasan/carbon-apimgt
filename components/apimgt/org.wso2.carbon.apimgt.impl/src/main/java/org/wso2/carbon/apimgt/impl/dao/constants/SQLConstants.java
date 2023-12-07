83b3fe25a8093ac3e5bd2468a2b622fa5d915ea7
Kannan Kirishikesan <kannan@wso2.com>
2023-12-07 15:45:12 +0530

    Generalises sql query for scope retrieval

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/constants/SQLConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/constants/SQLConstants.java
@@ -2434,12 +2434,20 @@ public class SQLConstants {
 
     public static final String GET_SCOPE_BY_SUBSCRIBED_ID_ORACLE_SQL =
             "SELECT DISTINCT ARSM.SCOPE_NAME " +
+                    "FROM AM_API_RESOURCE_SCOPE_MAPPING ARSM INNER JOIN AM_API_URL_MAPPING AUM " +
+                    "ON ARSM.URL_MAPPING_ID = AUM.URL_MAPPING_ID " +
+                    "WHERE AUM.API_ID IN (";
+
+    public static final String GET_SCOPE_BY_SUBSCRIBED_ID_SQL =
+            "SELECT DISTINCT ARSM.SCOPE_NAME " +
                 "FROM AM_SUBSCRIBER SUB " +
                 "INNER JOIN AM_APPLICATION APP ON SUB.SUBSCRIBER_ID = APP.SUBSCRIBER_ID " +
                 "INNER JOIN AM_SUBSCRIPTION SUBS ON APP.APPLICATION_ID = SUBS.APPLICATION_ID " +
-                "INNER JOIN AM_API API ON API.API_ID = SUBS.API_ID " +
-                "INNER JOIN AM_API_PRODUCT_MAPPING APM ON APM.API_ID = API.API_ID " +
-                "INNER JOIN AM_API_URL_MAPPING AUM ON AUM.URL_MAPPING_ID = apm.URL_MAPPING_ID " +
+                "INNER JOIN AM_API API ON API.API_ID = SUBS.API_ID " +    // Correct until this
+                "LEFT JOIN AM_API_PRODUCT_MAPPING APM ON APM.API_ID = API.API_ID " +
+                "INNER JOIN AM_API_URL_MAPPING AUM " +
+                    "ON AUM.URL_MAPPING_ID = APM.URL_MAPPING_ID " +
+                    "OR AUM.API_ID = API.API_ID " +
                 "INNER JOIN AM_API_RESOURCE_SCOPE_MAPPING ARSM ON ARSM.URL_MAPPING_ID = AUM.URL_MAPPING_ID " +
                 "WHERE SUB.TENANT_ID = ?  AND APP.APPLICATION_ID = ? " +
                 "AND SUBS.SUBS_CREATE_STATE = '" + APIConstants.SubscriptionCreatedStatus.SUBSCRIBE + "'";
