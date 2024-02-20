c26507e9c2cf90ac10e4a3f1915cbba202953bcd
hisanhunais <hisan.live@gmail.com>
2024-02-20 17:51:34 +0530

    Add java doc comments to the new methods

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OASParserUtil.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/definitions/OASParserUtil.java
@@ -631,6 +631,12 @@ public class OASParserUtil {
         }
     }
 
+    /**
+     * Process a given response entry of the API definition.
+     *
+     * @param response  The response object which needs to be processed.
+     * @param context The SwaggerUpdateContext object containing the context of the API definition.
+     */
     private static void setRefOfApiResponse(ApiResponse response, SwaggerUpdateContext context) {
         Content content = response.getContent();
         if (content != null) {
@@ -657,6 +663,12 @@ public class OASParserUtil {
         }
     }
 
+    /**
+     * Process a given response header entry of the API definition.
+     *
+     * @param header  The header object which needs to be processed.
+     * @param context The SwaggerUpdateContext object containing the context of the API definition.
+     */
     private static void setRefOfApiResponseHeader(Header header, SwaggerUpdateContext context) {
         Content content = header.getContent();
         if (content != null) {
@@ -688,6 +700,12 @@ public class OASParserUtil {
         }
     }
 
+    /**
+     * Process a given example entry of the API definition.
+     *
+     * @param example  The example object which needs to be processed.
+     * @param context The SwaggerUpdateContext object containing the context of the API definition.
+     */
     private static void setRefOfExample(Example example, SwaggerUpdateContext context) {
         String ref = example.get$ref();
         if (ref != null) {
