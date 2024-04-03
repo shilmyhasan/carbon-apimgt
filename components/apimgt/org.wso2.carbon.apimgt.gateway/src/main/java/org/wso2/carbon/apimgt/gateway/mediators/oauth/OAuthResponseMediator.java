f8a7ed078fc33b46901f657f30037723fa4318d6
dulith <dulithcsenanayake@gmail.com>
2024-04-03 15:39:48 +0530

    Mark OAuthResponseMediator as content-not-aware Fixes https://github.com/wso2-enterprise/wso2-apim-internal/issues/5938

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/oauth/OAuthResponseMediator.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/mediators/oauth/OAuthResponseMediator.java
@@ -72,6 +72,11 @@ public class OAuthResponseMediator extends AbstractMediator implements ManagedLi
         return true;
     }
 
+    @Override
+    public boolean isContentAware() {
+        return false;
+    }
+
     /**
      * Sends a fault response to the DevPortal console
      *
