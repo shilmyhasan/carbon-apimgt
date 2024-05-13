6610b6fc166f80af0a1c558d24ef24113a7c1653
msm1992 <madusha.sachini@yahoo.com>
2024-05-13 09:51:22 +0530

    Remove extra line

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIConstants.java
@@ -3013,6 +3013,5 @@ public final class APIConstants {
     // Property for enabling API level policy support feature
     public static final String ENABLE_API_POLICIES = "EnableAPIPolicies";
 
-
     public static final String EAGER_LOADING_ENABLED_TENANTS = "Tenant.LoadingPolicy.EagerLoading.Include";
 }
