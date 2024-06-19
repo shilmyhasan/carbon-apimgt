126ef3f5cbd132b55a8d1527a4e6cf94c2a45ed5
piyumaldk <piyumaldk@gmail.com>
2024-06-19 19:39:08 +0530

    Close input stream properly

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -26,6 +26,7 @@ import org.apache.axiom.om.OMException;
 import org.apache.axiom.om.util.AXIOMUtil;
 import org.apache.axis2.Constants;
 import org.apache.axis2.util.JavaUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.collections.MapUtils;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringEscapeUtils;
@@ -8882,11 +8883,12 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
     @Override
     public void setThumbnailToAPI(String apiId, ResourceFile resource, String organization) throws APIManagementException {
 
+        InputStream thumbnailImageContent = null;
         try {
             org.wso2.carbon.apimgt.persistence.dto.ResourceFile iconResourceFile = new org.wso2.carbon.apimgt.persistence.dto.ResourceFile(
                     resource.getContent(), resource.getContentType());
-            InputStream content = iconResourceFile.getContent();
-            if (content.available() > 0) {
+            thumbnailImageContent = iconResourceFile.getContent();
+            if (thumbnailImageContent.available() > 0) {
                 apiPersistenceInstance.saveThumbnail(new Organization(organization), apiId, iconResourceFile);
             } else {
                 // Thumbnail deletion from publisher originally handled through the same PUT call
@@ -8901,6 +8903,8 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
             }
         } catch (IOException e) {
             throw new APIManagementException("Error while reading input stream ", e);
+        } finally {
+            IOUtils.closeQuietly(thumbnailImageContent);
         }
     }
 
