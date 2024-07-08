1e22c2f219f1d3db777d8fb0c8b3ccb7ec1137e5
tharikaGitHub <tharika.m92@gmail.com>
2024-07-08 11:01:57 +0530

    Remove unused code

--- components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/service/APIGatewayAdmin.java
+++ components/apimgt/org.wso2.carbon.apimgt.gateway/src/main/java/org/wso2/carbon/apimgt/gateway/service/APIGatewayAdmin.java
@@ -42,8 +42,6 @@ import org.wso2.carbon.rest.api.ResourceData;
 import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -551,37 +549,7 @@ public class APIGatewayAdmin extends org.wso2.carbon.core.AbstractAdmin {
      * @throws AxisFault
      */
     public boolean deployPolicy(String content, String fileName) throws AxisFault {
-
-        File file = new File(APIConstants.POLICY_FILE_FOLDER);      //WSO2Carbon_Home/repository/deployment/server
-        // /throttle-config
-        //if directory doesn't exist, make onee
-        if (!file.exists()) {
-            file.mkdir();
-        }
-        File writeFile = new File(APIConstants.POLICY_FILE_LOCATION + fileName + APIConstants.XML_EXTENSION);  //file
-        // folder+/
-        FileOutputStream fos = null;
-        try {
-            fos = new FileOutputStream(writeFile);
-            //if file doesn't exit make one
-            if (!writeFile.exists()) {
-                writeFile.createNewFile();
-            }
-            byte[] contentInBytes = content.getBytes();
-            fos.write(contentInBytes);
-            fos.flush();
-            return true;
-        } catch (IOException e) {
-            log.error("Error occurred writing to " + fileName + ":", e);
-        } finally {
-            try {
-                if (fos != null) {
-                    fos.close();
-                }
-            } catch (IOException e) {
-                log.error("Error occurred closing file output stream", e);
-            }
-        }
+        // Do nothing
         return false;
     }
 
@@ -591,17 +559,8 @@ public class APIGatewayAdmin extends org.wso2.carbon.core.AbstractAdmin {
      * @param fileNames file names to be deleted
      */
     public boolean undeployPolicy(String[] fileNames) {
-
-        for (int i = 0; i < fileNames.length; i++) {
-            File file = new File(APIConstants.POLICY_FILE_LOCATION + fileNames[i] + APIConstants.XML_EXTENSION);
-            boolean deleted = file.delete();
-            if (deleted) {
-                log.info("File : " + fileNames[i] + " is deleted");
-            } else {
-                log.error("Error occurred in deleting file: " + fileNames[i]);
-            }
-        }
-        return true;
+        // Do nothing
+        return false;
     }
 
     /**
