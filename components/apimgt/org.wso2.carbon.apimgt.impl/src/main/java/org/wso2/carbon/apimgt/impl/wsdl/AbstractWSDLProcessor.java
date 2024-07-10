d61d6899c6c0c81019b8ad1e970ce0e225f3228e
rusirijayodaillesinghe <rusiri@wso2.com>
2024-07-10 14:41:57 +0530

    [APIM 4.1.0] Fix Improper Restriction of XML External Entity Reference Fixes https://github.com/wso2-enterprise/wso2-apim-internal/issues/6800 [Entry ids: 1797, 1798]

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/wsdl/AbstractWSDLProcessor.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/wsdl/AbstractWSDLProcessor.java
@@ -64,6 +64,12 @@ abstract class AbstractWSDLProcessor implements WSDLProcessor {
         InputStream inputStream = null;
         try {
             DocumentBuilderFactory factory = getSecuredDocumentBuilder();
+            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            factory.setNamespaceAware(true);
+            factory.setFeature(Constants.SAX_FEATURE_PREFIX + Constants.EXTERNAL_GENERAL_ENTITIES_FEATURE,
+                    false);
+            factory.setFeature(Constants.SAX_FEATURE_PREFIX +
+                    Constants.EXTERNAL_PARAMETER_ENTITIES_FEATURE, false);
             DocumentBuilder builder = factory.newDocumentBuilder();
             inputStream = url.openStream();
             return builder.parse(inputStream);
@@ -86,6 +92,12 @@ abstract class AbstractWSDLProcessor implements WSDLProcessor {
         InputStream inputStream = null;
         try {
             DocumentBuilderFactory factory = getSecuredDocumentBuilder();
+            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            factory.setNamespaceAware(true);
+            factory.setFeature(Constants.SAX_FEATURE_PREFIX + Constants.EXTERNAL_GENERAL_ENTITIES_FEATURE,
+                    false);
+            factory.setFeature(Constants.SAX_FEATURE_PREFIX +
+                    Constants.EXTERNAL_PARAMETER_ENTITIES_FEATURE, false);
             DocumentBuilder builder = factory.newDocumentBuilder();
             inputStream = new FileInputStream(new File(path));
             return builder.parse(inputStream);
@@ -107,6 +119,12 @@ abstract class AbstractWSDLProcessor implements WSDLProcessor {
         InputStream inputStream = null;
         try {
             DocumentBuilderFactory factory = getSecuredDocumentBuilder();
+            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            factory.setNamespaceAware(true);
+            factory.setFeature(Constants.SAX_FEATURE_PREFIX + Constants.EXTERNAL_GENERAL_ENTITIES_FEATURE,
+                    false);
+            factory.setFeature(Constants.SAX_FEATURE_PREFIX +
+                    Constants.EXTERNAL_PARAMETER_ENTITIES_FEATURE, false);
             DocumentBuilder builder = factory.newDocumentBuilder();
             inputStream = new ByteArrayInputStream(content);
             return builder.parse(inputStream);
