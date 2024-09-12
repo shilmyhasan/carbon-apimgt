fe86120d9a2b9f5615869c307d27be1fe2a7c4ab
Ashan Rathnaweera <ashanahr@gmail.com>
2024-09-12 12:10:56 +0530

    Improve filtering block conditions by adding support to filter by exact condition value

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/constants/SQLConstantsH2MySQL.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/dao/constants/SQLConstantsH2MySQL.java
@@ -112,6 +112,9 @@ public class SQLConstantsH2MySQL extends SQLConstants{
     public static final String GET_BLOCK_CONDITIONS_BY_TYPE_AND_VALUE_SQL =
             "SELECT CONDITION_ID, TYPE, `VALUE`, ENABLED, DOMAIN, UUID FROM AM_BLOCK_CONDITIONS WHERE " +
                     "(TYPE = ? OR ? IS NULL) AND (`VALUE` LIKE CONCAT('%', ?, '%') OR ? IS NULL) AND DOMAIN = ?";
+    public static final String GET_BLOCK_CONDITIONS_BY_TYPE_AND_EXACT_VALUE_SQL =
+            "SELECT CONDITION_ID, TYPE, `VALUE`, ENABLED, DOMAIN, UUID FROM AM_BLOCK_CONDITIONS WHERE " +
+                    "(TYPE = ? OR ? IS NULL) AND `VALUE` = ? AND DOMAIN = ?";
     public static final String GET_BLOCK_CONDITION_SQL =
             "SELECT TYPE,`VALUE`,ENABLED,DOMAIN,UUID FROM AM_BLOCK_CONDITIONS WHERE CONDITION_ID =?";
     public static final String GET_BLOCK_CONDITION_BY_UUID_SQL =
