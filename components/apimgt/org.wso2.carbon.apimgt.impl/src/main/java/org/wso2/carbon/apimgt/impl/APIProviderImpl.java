579f1e24a507ebb356a346f57a50945e2e70cae3
senthuran16 <senthuranrc16@gmail.com>
2025-02-18 22:26:35 +0530

    Allow policy name matching with "_imported" suffix

--- components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
+++ components/apimgt/org.wso2.carbon.apimgt.impl/src/main/java/org/wso2/carbon/apimgt/impl/APIProviderImpl.java
@@ -1675,8 +1675,9 @@ class APIProviderImpl extends AbstractAPIManager implements APIProvider {
                                 ExceptionCodes.INVALID_OPERATION_POLICY);
                     }
 
-                    if (!policyData.getSpecification().getName()
-                            .equals(policy.getPolicyName()) || !policyData.getSpecification().getVersion()
+                    boolean isPolicyNameMatch = policyData.getSpecification().getName().equals(policy.getPolicyName())
+                            || (policyData.getSpecification().getName()).equals(policy.getPolicyName() + "_imported");
+                    if (!isPolicyNameMatch || !policyData.getSpecification().getVersion()
                             .equals(policy.getPolicyVersion())) {
                         throw new APIManagementException("Applied policy " + policy.getPolicyName() + "_"
                                 + policy.getPolicyVersion() + " does not match the specification");
